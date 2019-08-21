832369cb8ed887927643a3275b12a898a0bb64e9
Evan Yamanishi <yamanishi1@gmail.com>
2019-08-21 17:18:04 -0400

    fix(ci): base must end in a slash

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
 	title: 'The Norton Design System',
 	description: 'Usage guides for the Norton Design System.',
 	base: (process.env.CI_PAGES_URL)
-		? process.env.CI_PAGES_URL.replace('https://wwnorton.gitlab.io', '')
+		? `${process.env.CI_PAGES_URL.replace('https://wwnorton.gitlab.io', '')}/`
 		: '/',
 	themeConfig: {
 		nav: [
