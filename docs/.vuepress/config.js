759aced5863d3588bc88f148b9927ef7e1d10ca7
Evan Yamanishi <yamanishi1@gmail.com>
2019-08-21 16:32:25 -0400

    docs: add nav and sidebar structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,24 @@
+const componentsSidebar = require('./sidebars/components');
+
 module.exports = {
 	title: 'The Norton Design System',
-	description: 'Usage guides for the Norton Design System\'s components.',
+	description: 'Usage guides for the Norton Design System.',
 	base: (process.env.CI_PAGES_URL)
 		? process.env.CI_PAGES_URL.replace('https://wwnorton.gitlab.io', '')
 		: '/',
+	themeConfig: {
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Components', link: '/components/' },
+		],
+		sidebar: {
+			'/components/': [
+				{
+					title: 'Components',
+					collapsable: false,
+					children: componentsSidebar,
+				},
+			],
+		},
+	},
 };
