4a7c3e0f6827a73e3ec223a49d745a7e8bc48683
Evan Yamanishi <yamanishi1@gmail.com>
2019-08-22 13:55:05 -0400

    docs: add seagull logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,16 @@ module.exports = {
 	base: process.env.VUEPRESS_BASE || '/',
 	title: 'The Norton Design System',
 	description: 'Usage guides for the Norton Design System.',
+	serviceWorker: true,
+	head: [
+		['link', { rel: 'icon', href: '/assets/images/seagull.svg' }],
+		['link', { rel: 'icon', href: '/assets/images/favicon.32x32.png' }],
+		['link', { rel: 'icon', href: '/assets/images/favicon.16x16.png' }],
+		['link', { rel: 'manifest', href: '/manifest.json' }],
+		['meta', { name: 'theme-color', content: '#2e445c' }],
+	],
 	themeConfig: {
+		logo: '/assets/images/seagull.svg',
 		nav: [
 			{ text: 'Home', link: '/' },
 			{ text: 'Components', link: '/components/' },
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,6 @@
 ---
 home: true
+heroImage: /assets/images/seagull.svg
 actionText: Components →
 actionLink: /components/
 footer: Copyright © 2019-present W. W. Norton & Company
