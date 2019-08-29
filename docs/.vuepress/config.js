7be0194a366e82f85019aa5fd529f04bf66d9f2a
Evan Yamanishi <yamanishi1@gmail.com>
2019-08-29 17:34:48 -0400

    docs: add zeplin link to nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,6 +16,7 @@ module.exports = {
 		nav: [
 			{ text: 'Home', link: '/' },
 			{ text: 'Components', link: '/components/' },
+			{ text: 'Zeplin', link: 'https://app.zeplin.io/project/5d66e28439bbe3139aa846ad' }
 		],
 		sidebar: {
 			'/components/': [
