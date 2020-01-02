d7ec06cf9fd4ebfc9a11ce4d7e619929453addd5
Hon Chih Chen <hchen@wwnorton.com>
2020-01-02 18:46:00 +0000

    feat(react): initial text field

---
+++ packages/react/src/components/input/BaseInput/index.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { noop } from '../../../utilities/events';
+
+export const ID = 'input';
+export const DICTIONARY = {
+	PREFIX: 'nds-',
+	LABEL: 'label',
+	INPUT: 'input',
+	COUNT: 'count',
+	TEXTFIELD: 'textfield',
+	HELP: 'help',
+	ERROR: 'error',
+	FEEDBACK: 'feedback',
+	INVALID: 'Not a valid input',
+	REQUIRED: 'required',
+	MAX_COUNT: 45,
+};
+
+export interface BaseInputProps
+	extends React.InputHTMLAttributes<HTMLInputElement> {
+	type: string;
+	ref?: React.Ref<HTMLInputElement>;
+	required?: boolean;
+	id?: string;
+	maxLength?: number;
+}
+
+export const defaultProps: Partial<BaseInputProps> = {
+	onChange: noop,
+};
+
+export const BaseInput: React.ComponentType<BaseInputProps> = React.forwardRef(
+	(props: BaseInputProps, ref?: React.Ref<HTMLInputElement>) => {
+		const {
+			type,
+			className,
+			...attributes
+		} = { ...defaultProps, ...props };
+
+		return (
+			<>
+				<ID
+					className={`${className}`}
+					type={type}
+					ref={ref}
+					{...attributes}
+				/>
+			</>
+		);
+	},
+);
