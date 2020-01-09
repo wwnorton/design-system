aff1bc90757a7a4b3dbb78181887c6f11219e804
Evan Yamanishi <yamanishi1@gmail.com>
2020-01-09 17:29:30 -0500

    docs(react): add global reset for storybook

--- packages/react/.storybook/config.js
+++ packages/react/.storybook/config.js
@@ -1,5 +1,6 @@
 import { addDecorator, configure } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import './globals.scss';
 
 configure(require.context('../src', true, /\.stories\.tsx?$/), module);
 addDecorator(withInfo);
