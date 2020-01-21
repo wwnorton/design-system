b4a7c2bf8691114ef2dee7e04cc84a378e69b57f
Gabriel Rumbaut <grumbaut@wwnorton.com>
2020-01-21 16:17:22 -0500

    test: add checkbox tests

--- packages/react/src/components/Checkbox/checkbox.test.tsx
+++ packages/react/src/components/Checkbox/checkbox.test.tsx
@@ -7,3 +7,21 @@ test('Checkbox: renders its defaults', (t) => {
 	const component = renderer.create(<Checkbox label="Checkbox" />);
 	t.snapshot(component.toJSON());
 });
+
+test('Checkbox: set `help` with string', (t) => {
+	const component = renderer.create(<Checkbox label="Checkbox" help="Help Text" />);
+	t.snapshot(component.toJSON());
+});
+
+test('Checkbox: set `help` with a JSX element', (t) => {
+	const Help = <p>Help text</p>;
+	const component = renderer.create(<Checkbox label="Checkbox" help={Help} />);
+	t.snapshot(component.toJSON());
+});
+
+test('Checkbox: displays error message', (t) => {
+	const component = renderer.create(<Checkbox label="Checkbox" error="Error" required validateOnChange />);
+	const input = component.root.findByType('input');
+	input.props.onChange();
+	t.snapshot(component.toJSON());
+});
