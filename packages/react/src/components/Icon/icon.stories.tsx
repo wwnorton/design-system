0447df84ab638cec582a86fee87ef0d089e077da
Evan Yamanishi <yamanishi1@gmail.com>
2020-02-11 12:36:18 -0500

    feat(react): initial icon

---
+++ packages/react/src/components/Icon/icon.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+	withKnobs,
+	number,
+	select,
+	text,
+} from '@storybook/addon-knobs';
+import '@nds/core/src/components/disclosure/index.scss';
+import Icon, { IconProps } from '.';
+
+export default {
+	title: 'Icon',
+	component: Icon,
+	decorators: [withKnobs],
+};
+
+const IconOptions: Record<string, IconProps['variant']> = {
+	'Caret right': 'caret-right',
+	'Chevron down': 'chevron-down',
+	Close: 'close',
+	Check: 'check',
+};
+
+export const Default = (): JSX.Element => (
+	<Icon
+		variant={select<IconProps['variant']>('Icon', IconOptions, 'caret-right')}
+		label={text('Label', undefined)}
+		width={number('Width', 48)}
+	/>
+);
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,5 +2,6 @@
 export { default as Button } from './components/Button';
 export { default as Checkbox } from './components/Checkbox';
 export { default as Disclosure } from './components/Disclosure';
+export { default as Icon } from './components/Icon';
 export { default as Switch } from './components/Switch';
 export { default as TextField } from './components/TextField';
