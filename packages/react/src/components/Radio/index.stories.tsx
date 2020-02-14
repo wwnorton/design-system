cc0dedbc700ec227fc3fca436111bf6a97db6594
Gabriel Rumbaut <grumbaut@wwnorton.com>
2020-02-14 17:04:28 -0500

    refactor: add WithThumbnail story to radio storybook
    I've also updated to include the required props from the state change and imported Sass.

--- packages/react/src/components/Radio/index.stories.tsx
+++ packages/react/src/components/Radio/index.stories.tsx
@@ -5,7 +5,18 @@ import {
 	boolean,
 	text,
 } from '@storybook/addon-knobs';
+import '@nds/core/src/components/radio/index.scss';
 import Radio from '.';
+
+const Thumbnail = (props: { className?: string }): JSX.Element => {
+	const { className } = props;
+	return (
+		// https://fontawesome.com/license
+		<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="book" className={className} role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+			<path fill="currentColor" d="M448 360V24c0-13.3-10.7-24-24-24H96C43 0 0 43 0 96v320c0 53 43 96 96 96h328c13.3 0 24-10.7 24-24v-16c0-7.5-3.5-14.3-8.9-18.7-4.2-15.4-4.2-59.3 0-74.7 5.4-4.3 8.9-11.1 8.9-18.6zM128 134c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm0 64c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm253.4 250H96c-17.7 0-32-14.3-32-32 0-17.6 14.4-32 32-32h285.4c-1.9 17.1-1.9 46.9 0 64z" />
+		</svg>
+	);
+};
 
 export default {
 	title: 'Radio',
@@ -19,5 +30,21 @@ export const Default = (): JSX.Element => (
 		help={text('Help', 'Additional information about this radio.')}
 		disabled={boolean('Disabled', false)}
 		onChange={action('onChange')}
+		selectedValue="Radio"
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		updateState={(): void => {}}
+	/>
+);
+
+export const WithThumbnail = (): JSX.Element => (
+	<Radio
+		label={text('Label', 'Radio')}
+		help={text('Help', 'Additional information about this radio.')}
+		disabled={boolean('Disabled', false)}
+		onChange={action('onChange')}
+		selectedValue="Radio"
+		thumbnail={<Thumbnail />}
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		updateState={(): void => {}}
 	/>
 );
