7fd7f95a09429431e9be391cb80cda2257018dd9
Evan Yamanishi <yamanishi1@gmail.com>
2020-02-28 13:10:11 -0500

    test(switch): add tests for aria-checked and state indictor

--- packages/react/src/components/Switch/switch.test.tsx
+++ packages/react/src/components/Switch/switch.test.tsx
@@ -7,3 +7,36 @@ test('renders its defaults', (t) => {
 	const component = renderer.create(<Switch />);
 	t.snapshot(component.toJSON());
 });
+
+test('the switch\'s state is reflected by aria-checked', (t) => {
+	const component = renderer.create(<Switch />);
+	const btn = component.root.findByType('button');
+	t.is(btn.props['aria-checked'], 'false');
+	t.snapshot(component.toJSON(), 'initial state');
+
+	btn.props.onClick();
+	t.is(btn.props['aria-checked'], 'true');
+	t.snapshot(component.toJSON(), 'after clicking');
+});
+
+test('the state indicator can be disabled', (t) => {
+	const component = renderer.create(<Switch displayState={false} />);
+	const spans = component.root.findAllByType('span');
+	t.is(spans.length, 0);
+});
+
+test('the state indicator can be customized', (t) => {
+	const props = {
+		on: 'On!',
+		off: 'Off!',
+	};
+	const component = renderer.create(<Switch {...props} />);
+	const btn = component.root.findByType('button');
+	const span = component.root.findByType('span');
+	t.is(span.props.children.toString(), props.off);
+	t.snapshot(component.toJSON(), 'initial state');
+
+	btn.props.onClick();
+	t.is(span.props.children.toString(), props.on);
+	t.snapshot(component.toJSON(), 'after clicking');
+});
