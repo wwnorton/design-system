152fccdd13998ac0649d23236cb287ef02ee9f78
Evan Yamanishi <yamanishi1@gmail.com>
2020-04-01 11:15:36 -0400

    feat(react): add hook to merge forwarded refs

--- packages/react/src/utilities/hooks.ts
+++ packages/react/src/utilities/hooks.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface UseActiveReturn {
 	active: boolean;
@@ -15,4 +15,23 @@ export const useActive = (initial: boolean): UseActiveReturn => {
 		activate: (): void => setActive(true),
 		deactivate: (): void => setActive(false),
 	};
+};
+
+export const useForwardedRef = <T>(forwardedRef: React.Ref<T>): React.RefObject<T> => {
+	const innerRef = useRef<T>(null);
+
+	useEffect(() => {
+		if (!forwardedRef) return;
+
+		if (typeof forwardedRef === 'function') {
+			forwardedRef(innerRef.current);
+		} else {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+			// @ts-ignore
+			// eslint-disable-next-line no-param-reassign
+			forwardedRef.current = innerRef.current;
+		}
+	});
+
+	return innerRef;
 };
