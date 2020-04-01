efad9e2ee28b93a05f4d71d4c7a2ec93da03e774
Evan Yamanishi <yamanishi1@gmail.com>
2020-04-01 12:28:08 -0400

    docs(storybook): update textfield stories

--- packages/react/src/components/TextField/index.stories.tsx
+++ packages/react/src/components/TextField/index.stories.tsx
@@ -6,7 +6,7 @@ import {
 	number,
 	text,
 } from '@storybook/addon-knobs';
-import '@nds/core/src/components/textfield/index.scss';
+import './index.stories.scss';
 import TextField from '.';
 
 export default {
@@ -17,36 +17,33 @@ export default {
 
 const { defaultProps } = TextField;
 
-export const Default = (): JSX.Element => (
+export const Default = (): React.ReactElement => (
 	<TextField
 		label={text('Label', 'Default Text Field')}
 		help='The default Text Field has a type of "text"'
 
-		constraintValidation={boolean('Constraint validation', true)}
-		includeDefaultValidators={boolean('Include default validators', true)}
-		onChangeNative={action('onChangeNative')}
+		onDOMChange={action('onDOMChange')}
 		onValidate={action('onValidate')}
-		required={boolean('Required', defaultProps.required as boolean)}
-		validateOnChange={boolean('Validate on React change (the DOM\'s oninput)', false)}
-		validateOnChangeNative={boolean('Validate on DOM change', true)}
+		required={boolean('Required', defaultProps.required)}
+		validateOnChange={boolean('Validate on React change', false)}
+		validateOnDOMChange={boolean('Validate on DOM change', true)}
 	/>
 );
 
-export const Email = (): JSX.Element => (
+export const Email = (): React.ReactElement => (
 	<TextField
 		type="email"
 		label="Email"
 
-		includeDefaultValidators={boolean('Include default validators', true)}
-		onChangeNative={action('onChangeNative')}
+		onDOMChange={action('onDOMChange')}
 		onValidate={action('onValidate')}
-		required={boolean('Required', defaultProps.required as boolean)}
-		validateOnChange={boolean('Validate on React change (the DOM\'s oninput)', false)}
-		validateOnChangeNative={boolean('Validate on DOM change', true)}
+		required={boolean('Required', defaultProps.required)}
+		validateOnChange={boolean('Validate on React change', false)}
+		validateOnDOMChange={boolean('Validate on DOM change', true)}
 	/>
 );
 
-export const Number = (): JSX.Element => (
+export const Number = (): React.ReactElement => (
 	<TextField
 		type="number"
 		label="Number"
@@ -54,71 +51,113 @@ export const Number = (): JSX.Element => (
 		min={number('Minimum', 2)}
 		max={number('Maximum', 8)}
 
-		includeDefaultValidators={boolean('Include default validators', true)}
-		onChangeNative={action('onChangeNative')}
+		onDOMChange={action('onDOMChange')}
 		onValidate={action('onValidate')}
-		required={boolean('Required', defaultProps.required as boolean)}
-		validateOnChange={boolean('Validate on React change (the DOM\'s oninput)', false)}
-		validateOnChangeNative={boolean('Validate on DOM change', true)}
+		required={boolean('Required', defaultProps.required)}
+		validateOnChange={boolean('Validate on React change', false)}
+		validateOnDOMChange={boolean('Validate on DOM change', true)}
 	/>
 );
 
-export const Password = (): JSX.Element => (
+export const Password = (): React.ReactElement => (
 	<TextField
 		type="password"
 		label="Password"
 		minLength={number('Minimum length', 8)}
 
-		includeDefaultValidators={boolean('Include default validators', true)}
-		onChangeNative={action('onChangeNative')}
+		onDOMChange={action('onDOMChange')}
 		onValidate={action('onValidate')}
-		required={boolean('Required', defaultProps.required as boolean)}
-		validateOnChange={boolean('Validate on React change (the DOM\'s oninput)', false)}
-		validateOnChangeNative={boolean('Validate on DOM change', true)}
+		required={boolean('Required', defaultProps.required)}
+		validateOnChange={boolean('Validate on React change', false)}
+		validateOnDOMChange={boolean('Validate on DOM change', true)}
 	/>
 );
 
-export const Telephone = (): JSX.Element => (
+export const Telephone = (): React.ReactElement => (
 	<TextField
 		type="tel"
 		label="Phone number"
 
-		includeDefaultValidators={boolean('Include default validators', true)}
-		onChangeNative={action('onChangeNative')}
+		onDOMChange={action('onDOMChange')}
 		onValidate={action('onValidate')}
-		required={boolean('Required', defaultProps.required as boolean)}
-		validateOnChange={boolean('Validate on React change (the DOM\'s oninput)', false)}
-		validateOnChangeNative={boolean('Validate on DOM change', true)}
+		required={boolean('Required', defaultProps.required)}
+		validateOnChange={boolean('Validate on React change', false)}
+		validateOnDOMChange={boolean('Validate on DOM change', true)}
 	/>
 );
 
-export const URL = (): JSX.Element => (
+export const URL = (): React.ReactElement => (
 	<TextField
 		type="url"
 		label="URL"
 
-		includeDefaultValidators={boolean('Include default validators', true)}
-		onChangeNative={action('onChangeNative')}
+		onDOMChange={action('onDOMChange')}
 		onValidate={action('onValidate')}
-		required={boolean('Required', defaultProps.required as boolean)}
-		validateOnChange={boolean('Validate on React change (the DOM\'s oninput)', false)}
-		validateOnChangeNative={boolean('Validate on DOM change', true)}
+		required={boolean('Required', defaultProps.required)}
+		validateOnChange={boolean('Validate on React change', false)}
+		validateOnDOMChange={boolean('Validate on DOM change', true)}
 	/>
 );
 
-export const withMaxLength = (): JSX.Element => (
+export const withMaxLength = (): React.ReactElement => (
 	<TextField
 		label="Text Field with max length"
 		maxLength={number('Maximum length', 10)}
 		counterStart={number('Start counter at', 8)}
 		onCount={action('onCount')}
 		validateOnInput
-
-		includeDefaultValidators={boolean('Include default validators', true)}
-		onChangeNative={action('onChangeNative')}
-		onValidate={action('onValidate')}
-		required={boolean('Required', defaultProps.required as boolean)}
-		validateOnChange={boolean('Validate on React change (the DOM\'s oninput)', false)}
-		validateOnChangeNative={boolean('Validate on DOM change', true)}
 	/>
 );
+
+interface CustomValidationProps { firstName: string; lastName: string }
+export const customValidation = (): React.ReactElement => {
+	const CustomValidation: React.FunctionComponent<CustomValidationProps> = ({
+		firstName,
+		lastName,
+	}: CustomValidationProps) => {
+		const inputRef = React.useRef<HTMLInputElement>();
+		const [value, setValue] = React.useState('');
+		const [isValid, setValidity] = React.useState(false);
+		const onChange = (e): void => {
+			setValue(e.target.value);
+			action('onChange')(e);
+		};
+		const onValidate = (state): void => {
+			if (state.errors.length === 0) {
+				setValidity(true);
+			} else {
+				setValidity(false);
+			}
+			action('onValidate')(state);
+		};
+		return (
+			<>
+				<TextField
+					inputRef={inputRef}
+					value={value}
+					onChange={onChange}
+
+					label="Full name"
+					help="Enter a value that begins with the first name and ends with the last name specified in the knobs below."
+					validators={[
+						{
+							test: (v: string): boolean => v.startsWith(firstName),
+							message: `Must begin with "${firstName}".`,
+						},
+						{
+							test: (v: string): boolean => v.endsWith(lastName),
+							message: `Must end with "${lastName}".`,
+						},
+					]}
+					feedback={(isValid) ? `Well done, ${value}!` : undefined}
+
+					onDOMChange={action('onDOMChange')}
+					onValidate={onValidate}
+					required
+					validateOnChange
+				/>
+			</>
+		);
+	};
+	return <CustomValidation firstName={text('First name', 'Jane')} lastName={text('Last name', 'Doe')} />;
+};
