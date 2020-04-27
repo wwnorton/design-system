8470d729c6e034c13b99e050517caeddf282a711
Evan Yamanishi <yamanishi1@gmail.com>
2020-04-27 14:12:12 -0400

    refactor(react): create function mergeRefs that can be used outside of hooks

--- packages/react/src/utilities/hooks.ts
+++ packages/react/src/utilities/hooks.ts
@@ -1,5 +1,5 @@
-/* eslint-disable import/prefer-default-export */
 import { useEffect, useRef, useState } from 'react';
+import { mergeRefs } from './helpers';
 
 interface UseActiveReturn {
 	active: boolean;
@@ -17,21 +17,12 @@ export const useActive = (initial: boolean): UseActiveReturn => {
 	};
 };
 
-export const useForwardedRef = <T>(forwardedRef: React.Ref<T>): React.RefObject<T> => {
+export const useForwardedRef = <T>(forwardedRef?: React.Ref<T>): React.RefObject<T> => {
 	const innerRef = useRef<T>(null);
 
 	useEffect(() => {
-		if (!forwardedRef) return;
-
-		if (typeof forwardedRef === 'function') {
-			forwardedRef(innerRef.current);
-		} else {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-			// @ts-ignore
-			// eslint-disable-next-line no-param-reassign
-			forwardedRef.current = innerRef.current;
-		}
-	});
+		mergeRefs(innerRef, forwardedRef);
+	}, [forwardedRef]);
 
 	return innerRef;
 };
