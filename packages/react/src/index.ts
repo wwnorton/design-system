ad4ac4a0de25ea5eeed135929a3ec65d8e0e5ff1
Evan Yamanishi <yamanishi1@gmail.com>
2020-04-30 12:47:23 -0400

    fix(react): export Modal as a top-level component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -5,6 +5,7 @@ export { default as Disclosure } from './components/Disclosure';
 export { default as Dropdown } from './components/Dropdown';
 export { default as Icon } from './components/Icon';
 export { default as IconButton } from './components/IconButton';
+export { default as Modal } from './components/Modal';
 export { default as MultipleChoice } from './components/MultipleChoice';
 export { default as Radio } from './components/Radio';
 export { default as Switch } from './components/Switch';
