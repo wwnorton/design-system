1adff2aee412b6126b31e7ceebd1920b135cece8
Evan Yamanishi <yamanishi1@gmail.com>
2020-05-01 20:23:46 -0400

    fix(react): use a real HTML comment in SVG source

--- packages/react/src/components/BaseSVG/index.tsx
+++ packages/react/src/components/BaseSVG/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForwardedRef } from '../../utilities/hooks';
 
 export interface BaseSVGProps extends React.SVGAttributes<SVGSVGElement> {
 	/** A URL or reference to the source of this SVG. */
@@ -9,11 +10,18 @@ const BaseSVG = React.forwardRef<SVGSVGElement, BaseSVGProps>(({
 	source,
 	children,
 	...attributes
-}: BaseSVGProps, ref) => (
-	<svg ref={ref} xmlns="http://www.w3.org/2000/svg" {...attributes}>
-		{source && `<!-- ${source} -->`}
-		{ children }
-	</svg>
-));
+}: BaseSVGProps, ref) => {
+	const svg = useForwardedRef(ref);
+	React.useEffect(() => {
+		if (svg.current && source) {
+			svg.current.prepend(document.createComment(source));
+		}
+	}, [source, svg]);
+	return (
+		<svg ref={svg} xmlns="http://www.w3.org/2000/svg" {...attributes}>
+			{ children }
+		</svg>
+	);
+});
 
 export default BaseSVG;
