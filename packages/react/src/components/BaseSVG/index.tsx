f955d2b2bd58a472e28d7ecc8cfdbedaf3468490
Evan Yamanishi <yamanishi1@gmail.com>
2020-05-01 20:38:51 -0400

    refactor(react): render svg source as a data-attribute

--- packages/react/src/components/BaseSVG/index.tsx
+++ packages/react/src/components/BaseSVG/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useForwardedRef } from '../../utilities/hooks';
 
 export interface BaseSVGProps extends React.SVGAttributes<SVGSVGElement> {
 	/** A URL or reference to the source of this SVG. */
@@ -10,18 +9,15 @@ const BaseSVG = React.forwardRef<SVGSVGElement, BaseSVGProps>(({
 	source,
 	children,
 	...attributes
-}: BaseSVGProps, ref) => {
-	const svg = useForwardedRef(ref);
-	React.useEffect(() => {
-		if (svg.current && source) {
-			svg.current.prepend(document.createComment(source));
-		}
-	}, [source, svg]);
-	return (
-		<svg ref={svg} xmlns="http://www.w3.org/2000/svg" {...attributes}>
-			{ children }
-		</svg>
-	);
-});
+}: BaseSVGProps, ref) => (
+	<svg
+		ref={ref}
+		xmlns="http://www.w3.org/2000/svg"
+		data-source={source}
+		{...attributes}
+	>
+		{ children }
+	</svg>
+));
 
 export default BaseSVG;
