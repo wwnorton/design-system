91b81bb5522d1d4a16b30b0eea45dd5bd936d285
Evan Yamanishi <yamanishi1@gmail.com>
2020-05-31 18:53:36 -0400

    chore(eslint): don't require test fixture return typing

--- packages/react/.eslintrc.js
+++ packages/react/.eslintrc.js
@@ -6,4 +6,12 @@ module.exports = {
 	env: {
 		browser: true,
 	},
+	overrides: [
+		{
+			files: ['*.test.ts', '*.test.tsx'],
+			rules: {
+				'@typescript-eslint/explicit-function-return-type': 'off',
+			},
+		},
+	],
 };
