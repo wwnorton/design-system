b0c55568220baab47259e093d28e0e67274ad712
Evan Yamanishi <yamanishi1@gmail.com>
2020-06-05 19:23:14 -0400

    chore: re-add ts paths
    docs will build @nds/{core,react} from source for use in its bundle.

--- docs/gatsby-node.js
+++ docs/gatsby-node.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 exports.onCreateWebpackConfig = ({ actions }) => {
 	actions.setWebpackConfig({
 		resolve: {
+			plugins: [
+				new TsconfigPathsPlugin({
+					configFile: path.resolve(__dirname, 'tsconfig.json'),
+				}),
+			],
 			alias: {
+				// use root react & react-dom
 				react: path.resolve(__dirname, '../node_modules/react'),
 				'react-dom': path.resolve(__dirname, '../node_modules/react-dom'),
 			},
