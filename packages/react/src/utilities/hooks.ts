3d05dc23032d3ec516992bba76136cdd08dfdd11
Evan Yamanishi <yamanishi1@gmail.com>
2020-06-18 18:26:37 -0400

    feat: add usePopper hook
    this hook creates and updates a popper.js instance.

--- packages/react/src/utilities/hooks.ts
+++ packages/react/src/utilities/hooks.ts
@@ -1,4 +1,18 @@
-import { useEffect, useRef } from 'react';
+import {
+	useEffect,
+	useLayoutEffect,
+	useMemo,
+	useRef,
+	useState,
+} from 'react';
+import {
+	createPopper,
+	Instance as PopperInstance,
+	Options as PopperOptions,
+	VirtualElement,
+} from '@popperjs/core';
+import uniqueId from 'lodash.uniqueid';
+import isEqual from 'react-fast-compare';
 
 export const useForwardedRef = <T>(forwardedRef?: React.Ref<T>): React.RefObject<T> => {
 	const innerRef = useRef<T>(null);
@@ -17,4 +31,75 @@ export const useForwardedRef = <T>(forwardedRef?: React.Ref<T>): React.RefObject
 	}, [forwardedRef]);
 
 	return innerRef;
+};
+
+export interface UsePopperProps extends PopperOptions {
+	/**
+	 * The reference element that the popper will be attached to.
+	 * @see https://popper.js.org/docs/v2/constructors/#createpopper
+	 */
+	reference?: Element | VirtualElement | null;
+	popper?: HTMLElement | null;
+	children?: React.ReactNode;
+}
+
+export const usePopper = ({
+	reference,
+	popper,
+	children,
+	placement = 'auto',
+	modifiers = [],
+	strategy = 'absolute',
+	onFirstUpdate,
+}: UsePopperProps): React.MutableRefObject<PopperInstance | null> => {
+	const instance = useRef<PopperInstance | null>(null);
+	const prevOptions = useRef<PopperOptions>({
+		placement, modifiers, strategy, onFirstUpdate,
+	});
+
+	// memoize the options, only updating when Popper.js options props change
+	const options = useMemo(() => {
+		const newOptions = {
+			placement, modifiers, strategy, onFirstUpdate,
+		};
+		if (isEqual(prevOptions.current, newOptions)) {
+			return prevOptions.current || newOptions;
+		}
+		prevOptions.current = newOptions;
+		return newOptions;
+	}, [placement, modifiers, strategy, onFirstUpdate]);
+
+	// create the popper instance
+	useLayoutEffect(() => {
+		let popperInstance: PopperInstance;
+
+		if (reference && popper) {
+			popperInstance = createPopper(reference, popper, options);
+			instance.current = popperInstance;
+		}
+
+		return (): void => {
+			if (popperInstance) {
+				popperInstance.destroy();
+				instance.current = null;
+			}
+		};
+	// only update when reference or popper change.
+	// options changes should trigger setOptions() in the options layout effect
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [reference, popper]);
+
+	// update options when they change
+	useLayoutEffect(() => {
+		if (instance.current) {
+			instance.current.setOptions(options);
+		}
+	}, [options]);
+
+	// update the instance when children change to ensure the popper resizes
+	useLayoutEffect(() => {
+		if (instance.current) instance.current.update();
+	}, [instance, children]);
+
+	return instance;
 };
