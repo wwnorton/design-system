a72dacd9c2b0e449ac992ea9ad7cd0a85afa167d
Evan Yamanishi <yamanishi1@gmail.com>
2020-06-19 12:39:55 -0400

    refactor(react); simplify icon sizing & labeling
    * icons should always be square so set height & width to always be equal via the size prop
    * remove `label` prop, as it already exists in the form of aria-label
    * move fill up to svg root

--- packages/react/src/components/Icon/index.tsx
+++ packages/react/src/components/Icon/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import classNames from 'classnames';
 import {
-	findIcon, IconVariant, SVGIcon, materialDefaults,
+	findIcon, IconVariant, SVGIcon, viewBox as defaultViewBox,
 } from '../../utilities';
 import { BaseSVG, BaseSVGProps } from '../BaseSVG';
 
 export interface IconProps extends BaseSVGProps {
 	/** The base class name according to BEM conventions */
 	baseName?: string;
-	/** A descriptive label for the icon. Will be set as the aria-label. */
-	label?: string;
 	/** The specific icon that you'd like to use. */
 	variant?: IconVariant;
 	/** A custom icon. Must contain the SVG path's `d` attribute at a minimum. */
@@ -28,38 +26,16 @@ export interface IconProps extends BaseSVGProps {
 
 export const Icon = React.forwardRef<SVGSVGElement, IconProps>(({
 	baseName = 'icon',
-	label,
-	variant,
-	height: heightProp,
-	width,
 	className,
+	color = 'currentColor',
 	icon: iconProp,
-	onClick,
+	size = '1.25rem',
+	variant,
 	warnOnClick = true,
+	onClick,
+	'aria-label': ariaLabel,
 	...attributes
 }: IconProps, ref) => {
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const svgRef = ref || React.useRef<SVGSVGElement>(null);
-	const [height, setHeight] = React.useState<typeof heightProp>();
-
-	React.useEffect(() => {
-		if (typeof svgRef === 'object' && svgRef.current) {
-			const heightAttr = svgRef.current.getAttribute('height');
-			if (heightProp) {
-				if (heightAttr !== heightProp) setHeight(heightProp);
-				return;
-			}
-			const cssHeight = parseInt(window.getComputedStyle(svgRef.current).height, 10);
-			if (cssHeight) {
-				setHeight(undefined);
-			} else if (!heightProp && !width) {
-				setHeight('1.25rem');
-			} else {
-				setHeight(heightProp);
-			}
-		}
-	}, [heightProp, svgRef, width]);
-
 	if (onClick && warnOnClick) {
 		// eslint-disable-next-line no-console
 		console.warn(
@@ -76,31 +52,31 @@ export const Icon = React.forwardRef<SVGSVGElement, IconProps>(({
 	}
 	const {
 		d,
-		name,
 		source,
-		viewBox = materialDefaults.viewBox,
+		viewBox = defaultViewBox,
 	} = icon;
 
 	const classes = classNames(baseName, {
-		[`${baseName}--${name}`]: name,
+		[`${baseName}--${variant}`]: variant,
 	}, className);
 
 	return (
 		<BaseSVG
-			ref={svgRef}
+			ref={ref}
 			source={source}
 			viewBox={viewBox}
-			height={height}
-			width={width}
+			height={size}
+			width={size}
 			className={classes}
-			aria-label={label || undefined}
-			aria-hidden={(!label) ? 'true' : undefined}
+			aria-label={ariaLabel}
+			aria-hidden={(!ariaLabel) ? 'true' : undefined}
 			focusable="false"
 			role="img"
+			fill={color}
 			onClick={onClick}
 			{...attributes}
 		>
-			<path fill="currentColor" d={d} />
+			<path d={d} />
 		</BaseSVG>
 	);
 });
