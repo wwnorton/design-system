08d44b89e1d82afb024dd1d7a3d7c231e331709c
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-02 15:19:12 -0400

    docs(storybook): use default namespace and full css

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -1,10 +1,7 @@
 import { addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import { configure } from '../src';
 import './globals.scss';
-
-configure({ namespace: null });
 
 addDecorator(withInfo);
 addDecorator(withA11y);
