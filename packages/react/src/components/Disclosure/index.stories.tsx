b758c36ef69786b5f91f38513c6c51cf49874760
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-08 13:40:47 -0400

    chore: remove disclosure story styling

--- packages/react/src/components/Disclosure/index.stories.tsx
+++ packages/react/src/components/Disclosure/index.stories.tsx
@@ -3,10 +3,8 @@ import { action } from '@storybook/addon-actions';
 import {
 	withKnobs,
 	text,
-	select,
 	boolean,
 } from '@storybook/addon-knobs';
-import './index.stories.scss';
 import { Disclosure } from '.';
 
 export default {
@@ -15,19 +13,36 @@ export default {
 	decorators: [withKnobs],
 };
 
-const variantOptions = {
-	None: undefined,
-	Panel: 'panel',
-};
-
-const contents = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae velit, quibusdam culpa, consequuntur quos voluptate esse explicabo ipsa perspiciatis illo molestias dolorem atque praesentium modi saepe hic suscipit, deserunt debitis.';
+const contents = [
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae',
+	'velit, quibusdam culpa, consequuntur quos voluptate esse explicabo ipsa',
+	'perspiciatis illo molestias dolorem atque praesentium modi saepe hic',
+	'suscipit, deserunt debitis.',
+].toString();
 
 const { defaultProps } = Disclosure;
 
 export const Default: React.FunctionComponent = () => (
 	<Disclosure
 		summary={text('Summary', 'More information')}
-		variant={select('Variant', variantOptions, undefined)}
+		animate={boolean('Animate', defaultProps.animate)}
+		onToggle={action('onToggle')}
+		onOpenStart={action('onOpenStart')}
+		onOpenCancel={action('onOpenCancel')}
+		onOpenEnd={action('onOpenEnd')}
+		onCloseStart={action('onCloseStart')}
+		onCloseCancel={action('onCloseCancel')}
+		onCloseEnd={action('onCloseEnd')}
+		open={boolean('Open', false)}
+	>
+		<p>{text('Contents', contents)}</p>
+	</Disclosure>
+);
+
+export const Panel: React.FunctionComponent = () => (
+	<Disclosure
+		summary={text('Summary', 'More information')}
+		variant="panel"
 		animate={boolean('Animate', defaultProps.animate)}
 		onToggle={action('onToggle')}
 		onOpenStart={action('onOpenStart')}
