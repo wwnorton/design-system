3e73e1b8e466810cba007905789ef399f8c32a11
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-10 13:41:45 -0400

    docs(storybook): update textfield examples

--- packages/react/src/components/TextField/index.stories.tsx
+++ packages/react/src/components/TextField/index.stories.tsx
@@ -24,6 +24,7 @@ export const Default: React.FunctionComponent = () => {
 		<TextField
 			label={text('Label', 'Default Text Field')}
 			description='The default Text Field has a type of "text"'
+			disabled={boolean('Disabled', false)}
 
 			onDOMChange={action('onDOMChange')}
 			required={boolean('Required', false)}
@@ -93,8 +94,8 @@ const hide: ButtonProps = {
 		d: 'M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z',
 	},
 };
-const invalid: IconProps = { variant: 'close', color: 'var(--nds-red-60, #c93f3c)' };
-const valid: IconProps = { variant: 'check', color: 'var(--nds-green-60, #228522)' };
+const invalid: IconProps = { variant: 'close', color: 'var(--error)', children: 'Invalid' };
+const valid: IconProps = { variant: 'check', color: 'var(--success)', children: 'Valid' };
 const neutralUser: IconProps = { variant: 'account' };
 const neutralPW: IconProps = { variant: 'info' };
 
@@ -165,7 +166,7 @@ export const LoginForm: React.FunctionComponent = () => {
 				onValidate={setUserErrors}
 				validateOnChange
 				required
-				addonBefore={<Icon className="addon-icon" {...neutralUser} />}
+				addonBefore={<Icon {...neutralUser} />}
 				ref={setUserField}
 			/>
 			<TextField
@@ -175,7 +176,7 @@ export const LoginForm: React.FunctionComponent = () => {
 				errors={errors(pwErrors)}
 				required
 				minLength={number('Minimum length', 8)}
-				maxLength={number('Maximum length', 10)}
+				maxLength={number('Maximum length', 32)}
 				onValidate={setPwErrors}
 				validateOnChange
 				addonBefore={<Icon className="addon-icon" {...pwIcon} />}
