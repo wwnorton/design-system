4ae691684603a49738405f3094d66563e391431c
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-10 16:16:53 -0400

    refactor: make IconButton a forwardRef

--- packages/react/src/components/Button/IconButton.tsx
+++ packages/react/src/components/Button/IconButton.tsx
@@ -7,16 +7,17 @@ export type IconButtonProps = Omit<ButtonProps, 'iconOnly' | 'iconRight'>;
  * Display an icon in a button. By default,`variant` is "ghost". Equivalent to
  * `<Button variant="ghost" iconOnly icon="...">...</Button>`.
  */
-export const IconButton: React.FunctionComponent<IconButtonProps> = ({
+export const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(({
 	variant = 'ghost',
 	children,
 	...props
-}: IconButtonProps) => (
+}: IconButtonProps, ref) => (
 	<Button
 		iconOnly
 		variant={variant}
+		ref={ref}
 		{...props}
 	>
 		{ children }
 	</Button>
-);
+));
