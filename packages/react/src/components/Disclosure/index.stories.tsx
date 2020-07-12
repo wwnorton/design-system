8558e195382ac88abf8123274f885de39dd9189b
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-12 18:15:24 -0400

    docs(storybook): add controlled disclosure example

--- packages/react/src/components/Disclosure/index.stories.tsx
+++ packages/react/src/components/Disclosure/index.stories.tsx
@@ -13,7 +13,7 @@ export default {
 	decorators: [withKnobs],
 };
 
-const contents = [
+const defaultContents = [
 	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae',
 	'velit, quibusdam culpa, consequuntur quos voluptate esse explicabo ipsa',
 	'perspiciatis illo molestias dolorem atque praesentium modi saepe hic',
@@ -35,7 +35,7 @@ export const Default: React.FunctionComponent = () => (
 		onCloseEnd={action('onCloseEnd')}
 		open={boolean('Open', false)}
 	>
-		<p>{text('Contents', contents)}</p>
+		<p>{text('Contents', defaultContents)}</p>
 	</Disclosure>
 );
 
@@ -53,6 +53,42 @@ export const Panel: React.FunctionComponent = () => (
 		onCloseEnd={action('onCloseEnd')}
 		open={boolean('Open', false)}
 	>
-		<p>{text('Contents', contents)}</p>
+		<p>{text('Contents', defaultContents)}</p>
 	</Disclosure>
 );
+
+export const Controlled: React.FunctionComponent = () => {
+	const { current: summaryText } = React.useRef('Random poem');
+	const [contents, setContents] = React.useState<React.ReactNode>();
+	const [summary, setSummary] = React.useState<string>(summaryText);
+
+	// load a random poem
+	const getContents = async (): Promise<void> => {
+		setSummary(`${summaryText} (loading...)`);
+		const [{ content, poet, title }] = await fetch('https://www.poemist.com/api/v1/randompoems')
+			.then((r) => r.json());
+		setContents((
+			<>
+				<h2>{ title }</h2>
+				<pre>{ content }</pre>
+				<div>
+					&mdash;
+					{' '}
+					{ poet.name }
+				</div>
+			</>
+		));
+		setSummary(summaryText);
+	};
+
+	return (
+		<Disclosure
+			summary={summary}
+			variant="panel"
+			onOpenStart={getContents}
+			onCloseEnd={(): void => setContents(undefined)}
+		>
+			{ contents }
+		</Disclosure>
+	);
+};
