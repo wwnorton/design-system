6108cf4c2dfb949cbcec799d2e189bf62130fd5f
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-12 21:24:07 -0400

    refactor: add all components; reorganize

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,15 +1,17 @@
+export * from './config';
+export * from './utilities';
+
+// Components
 export * from './components/Button';
 export * from './components/Checkbox';
-export * from './components/Choice';
+export * from './components/ChoiceField';
 export * from './components/Disclosure';
 export * from './components/Dropdown';
+export * from './components/Field';
 export * from './components/Icon';
+export * from './components/LiveRegion';
 export * from './components/Modal';
-export * from './components/MultipleChoice';
 export * from './components/Radio';
 export * from './components/Switch';
 export * from './components/TextField';
 export * from './components/Tooltip';
-
-export * from './config';
-export * from './utilities';
