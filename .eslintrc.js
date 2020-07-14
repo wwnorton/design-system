aa544250e47f83032f76ec8edbe9a7bb2260e54b
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-14 15:29:23 -0400

    chore(deps): migrate overrides to eslint-config-norton

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,42 +39,6 @@ module.exports = {
 		],
 	},
 	overrides: [
-		// TypeScript
-		{
-			files: ['*.ts', '*.tsx'],
-			rules: {
-				indent: 'off',
-				'no-tabs': 'off',
-				'@typescript-eslint/indent': ['error', 'tab'],
-				'@typescript-eslint/no-empty-interface': 'off',
-			},
-		},
-		// React
-		{
-			files: ['*.jsx', '*.tsx'],
-			env: { browser: true },
-			plugins: ['react-hooks'],
-			rules: {
-				'react/jsx-indent': ['error', 'tab'],
-				'react/jsx-indent-props': ['error', 'tab'],
-				'react/jsx-filename-extension': [
-					'error',
-					{
-						extensions: ['.jsx', '.tsx'],
-					},
-				],
-				'react/jsx-props-no-spreading': ['off'],
-				'react/static-property-placement': [
-					'error',
-					'property assignment',
-					{
-						defaultProps: 'static public field',
-					},
-				],
-				'react-hooks/rules-of-hooks': 'error',
-				'react-hooks/exhaustive-deps': 'warn',
-			},
-		},
 		// Storybook
 		{
 			files: ['*.stories.tsx'],
