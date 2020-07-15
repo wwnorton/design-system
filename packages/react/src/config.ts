eb349ceea41c5e0f7519855c3a21de57d2c4c4ae
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-15 09:15:46 -0400

    refactor(react): add means to reset the config

--- packages/react/src/config.ts
+++ packages/react/src/config.ts
@@ -2,16 +2,26 @@ interface NDSConfig {
 	namespace: string | null;
 }
 
-class Config implements NDSConfig {
-	public namespace: NDSConfig['namespace'] = 'nds';
+export class Config implements NDSConfig {
+	public namespace: NDSConfig['namespace'] = Config.defaults.namespace;
+
+	public static defaults: Required<NDSConfig> = {
+		namespace: 'nds',
+	}
 
 	public configure(config: Partial<NDSConfig>): this {
 		if (config.namespace !== undefined) {
 			this.namespace = config.namespace;
 		}
+
 		return this;
+	}
+
+	public reset(): void {
+		Object.assign(this, Config.defaults);
 	}
 }
 
 export const config = new Config();
 export const configure = config.configure.bind(config);
+export const reset = config.reset.bind(config);
