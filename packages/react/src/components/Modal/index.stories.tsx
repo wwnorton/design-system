2b54ae297c5b937307b3b42c4787d97d05da8053
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-21 14:46:59 -0400

    refactor(react): create shared test/story fixtures for Modal

--- packages/react/src/components/Modal/index.stories.tsx
+++ packages/react/src/components/Modal/index.stories.tsx
@@ -6,76 +6,13 @@ import {
 import { Modal } from '.';
 import { Button } from '../Button';
 import { TextField } from '../TextField';
+import { shortContent, longContent } from './modal.fixtures';
 
 export default {
 	title: 'Modal',
 	component: Modal,
 	decorators: [withKnobs],
 };
-
-const shortContent = (
-	<p>
-		Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-		Id cum reiciendis sed ab voluptas velit quibusdam expedita sapiente nemo?
-		Modi accusamus minus distinctio error non! Provident quasi officia pariatur fugit.
-	</p>
-);
-
-const line = { style: { display: 'block' } };
-
-const longContent = (
-	<>
-		{ shortContent }
-		<p>
-			<span {...line}>Whan that Aprille with his shoures soote,</span>
-			<span {...line}>The droghte of March hath perced to the roote,</span>
-			<span {...line}>And bathed every veyne in swich licóur</span>
-			<span {...line}>Of which vertú engendred is the flour;</span>
-			<span {...line}>Whan Zephirus eek with his swete breeth</span>
-			<span {...line}>Inspired hath in every holt and heeth</span>
-			<span {...line}>The tendre croppes, and the yonge sonne</span>
-			<span {...line}>Hath in the Ram his halfe cours y-ronne,</span>
-			<span {...line}>And smale foweles maken melodye,</span>
-			<span {...line}>That slepen al the nyght with open ye,</span>
-			<span {...line}>So priketh hem Natúre in hir corages,</span>
-			<span {...line}>Thanne longen folk to goon on pilgrimages,</span>
-			<span {...line}>And palmeres for to seken straunge strondes,</span>
-			<span {...line}>To ferne halwes, kowthe in sondry londes;</span>
-			<span {...line}>And specially, from every shires ende</span>
-			<span {...line}>Of Engelond, to Caunterbury they wende,</span>
-			<span {...line}>The hooly blisful martir for to seke,</span>
-			<span {...line}>That hem hath holpen whan that they were seeke.</span>
-		</p>
-		<p>
-			<span {...line}>Bifil that in that seson on a day,</span>
-			<span {...line}>In Southwerk at the Tabard as I lay,</span>
-			<span {...line}>Redy to wenden on my pilgrymage</span>
-			<span {...line}>To Caunterbury with ful devout corage,</span>
-			<span {...line}>At nyght were come into that hostelrye</span>
-			<span {...line}>Wel nyne and twenty in a compaignye</span>
-			<span {...line}>Of sondry folk, by áventure y-falle</span>
-			<span {...line}>In felaweshipe, and pilgrimes were they alle,</span>
-			<span {...line}>That toward Caunterbury wolden ryde.</span>
-			<span {...line}>The chambres and the stables weren wyde,</span>
-			<span {...line}>And wel we weren esed atte beste.</span>
-			<span {...line}>And shortly, whan the sonne was to reste,</span>
-			<span {...line}>So hadde I spoken with hem everychon,</span>
-			<span {...line}>That I was of hir felaweshipe anon,</span>
-			<span {...line}>And made forward erly for to ryse,</span>
-			<span {...line}>To take oure wey, ther as I yow devyse.</span>
-		</p>
-		<p>
-			<span {...line}>But nathelees, whil I have tyme and space,</span>
-			<span {...line}>Er that I ferther in this tale pace,</span>
-			<span {...line}>Me thynketh it acordaunt to resoun</span>
-			<span {...line}>To telle yow al the condicioun</span>
-			<span {...line}>Of ech of hem, so as it semed me,</span>
-			<span {...line}>And whiche they weren and of what degree,</span>
-			<span {...line}>And eek in what array that they were inne;</span>
-			<span {...line}>And at a Knyght than wol I first bigynne.</span>
-		</p>
-	</>
-);
 
 const { defaultProps } = Modal;
 
