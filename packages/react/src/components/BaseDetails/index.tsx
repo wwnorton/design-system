f9ba9b54eeabd323dbe915e407c112c06cb3546d
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-28 12:47:07 -0400

    refactor(react): simplify BaseDetails
    onToggle now exists on the React definition; `open` can be passed directly to the `<details>`

--- packages/react/src/components/BaseDetails/index.tsx
+++ packages/react/src/components/BaseDetails/index.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 
-export interface BaseDetailsProps extends React.DetailsHTMLAttributes<HTMLDetailsElement> {
-	/**
-	 * A polyfill of the `ontoggle` event, which _does_ work but doesn't currently
-	 * exist in the `React.DetailsHTMLAttributes<HTMLDetailsElement>` definition.
-	 */
-	onToggle?: (e: React.SyntheticEvent<HTMLDetailsElement, Event>) => void;
-}
+export type BaseDetailsProps = React.DetailsHTMLAttributes<HTMLDetailsElement>;
 
 export const BaseDetails = React.forwardRef<HTMLDetailsElement, BaseDetailsProps>(({
-	open = false,
 	children,
 	...attributes
-}: BaseDetailsProps, ref) => {
-	const [isOpen, setOpen] = React.useState(open);
-	React.useEffect(() => setOpen(open), [open]);
-
-	return (
-		<details ref={ref} open={isOpen} {...attributes}>
-			{ children }
-		</details>
-	);
-});
+}: BaseDetailsProps, ref) => (
+	<details ref={ref} {...attributes}>
+		{ children }
+	</details>
+));
