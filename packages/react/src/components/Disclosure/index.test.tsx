e61fef5af9b6d21d3a9622c3f4ab473e48a2918b
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-29 12:08:35 -0400

    test(react): add initial disclosure tests
    not much to test here but for whatever reason clicking the summary does not open the details.

--- packages/react/src/components/Disclosure/index.test.tsx
+++ packages/react/src/components/Disclosure/index.test.tsx
@@ -2,10 +2,41 @@ import test from 'ava';
 import React from 'react';
 import {
 	cleanup, render, fireEvent, screen,
-	queryByRole, queryByText, queryByLabelText,
 } from '@testing-library/react';
 import { Disclosure } from '.';
 
 test.afterEach(cleanup);
 
-test.todo('write tests');
+const defaultSummary = 'More information';
+const shortContent = 'lorem ipsum';
+
+// TODO: investigate why summary clicks don't work in this testing context. They
+// do work in Storybook/real browsers.
+
+test.failing('clicking the summary opens a closed disclosure', async (t) => {
+	render(<Disclosure summary={defaultSummary}>{ shortContent }</Disclosure>);
+	const details = screen.getByRole('group') as HTMLDetailsElement;
+	const summary = screen.getByText(defaultSummary);
+	fireEvent.click(summary);
+	t.is(details.open, true);
+});
+
+test.failing('clicking the summary closes an open disclosure', async (t) => {
+	render(<Disclosure summary={defaultSummary} open>{ shortContent }</Disclosure>);
+	const details = screen.getByRole('group') as HTMLDetailsElement;
+	const summary = screen.getByText(defaultSummary);
+	fireEvent.click(summary);
+	t.is(details.open, false);
+});
+
+test('returning false on a callback cancels the callback', (t) => {
+	render((
+		<Disclosure summary={defaultSummary} onOpenStart={() => false}>
+			{ shortContent }
+		</Disclosure>
+	));
+	const summary = screen.getByText(defaultSummary);
+	fireEvent.click(summary);
+	const details = screen.getByRole('group') as HTMLDetailsElement;
+	t.is(details.open, false);
+});
