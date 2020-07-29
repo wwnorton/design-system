cce72f633aba9d23bd1c00c297baf797b5d1e593
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-29 11:27:47 -0400

    test(react): add initial icon tests

--- packages/react/src/components/Icon/index.test.tsx
+++ packages/react/src/components/Icon/index.test.tsx
@@ -2,10 +2,39 @@ import test from 'ava';
 import React from 'react';
 import {
 	cleanup, render, fireEvent, screen,
-	queryByRole, queryByText, queryByLabelText,
 } from '@testing-library/react';
 import { Icon } from '.';
 
 test.afterEach(cleanup);
 
-test.todo('write tests');
+test('contents are used for the icon\'s accessible name', (t) => {
+	const contents = 'foo bar';
+	render(<Icon variant="account">{ contents }</Icon>);
+	t.is(screen.getByLabelText(contents), screen.getByRole('img'));
+});
+
+test('contents are included in a tooltip when hovered', (t) => {
+	const contents = 'foo bar';
+	render(<Icon variant="account">{ contents }</Icon>);
+	const icon = screen.getByRole('img');
+	fireEvent.pointerEnter(icon);
+	const tooltip = screen.getByRole('tooltip', { hidden: true });
+	t.truthy(tooltip);
+	t.is(tooltip.textContent, contents);
+	t.is(icon.getAttribute('aria-labelledby'), tooltip.id);
+});
+
+test('icons are focusable when they have contents', (t) => {
+	const contents = 'foo bar';
+	render(<Icon variant="account">{ contents }</Icon>);
+	const icon = screen.getByRole('img');
+	t.true(icon.hasAttribute('tabindex'));
+});
+
+test('click listeners cannot be attached to icons', (t) => {
+	let clicked = false;
+	render(<Icon variant="account" onClick={() => { clicked = true; }} />);
+	const icon = screen.getByRole('img', { hidden: true });
+	fireEvent.click(icon);
+	t.false(clicked);
+});
