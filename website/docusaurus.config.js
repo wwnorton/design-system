bb6490ce367199aa02d309fc2fe7c5af6cdd28a2
Evan Yamanishi <yamanishi1@gmail.com>
2020-08-06 20:00:31 -0400

    docs: add lunr search

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -111,6 +111,7 @@ module.exports = {
 	themes: ['@docusaurus/theme-live-codeblock'],
 	plugins: [
 		path.resolve(__dirname, 'tsDocGen'),
+		'docusaurus-lunr-search',
 		[
 			'docusaurus-plugin-sass',
 			{
