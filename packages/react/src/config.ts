17bca757444dfaf3f1e0d05232f328b33872e4d6
Evan Yamanishi <yamanishi1@gmail.com>
2020-08-14 15:40:06 -0400

    feat(react): add persistent client storage
    This will form the basis for theme context storage, such as color scheme.

--- packages/react/src/config.ts
+++ packages/react/src/config.ts
@@ -1,27 +1,43 @@
-interface NDSConfig {
+import localForage from 'localforage';
+
+export interface NDSConfig {
+	/** The namespace is used to prefix default classNames. */
 	namespace: string | null;
+	/**
+	 * The fallback color scheme. This will only be used if the following are true:
+	 * 1. A `colorScheme` value is not found in the client storage.
+	 * 2. The user's operating system doesn't expose a preferred color scheme.
+	 *
+	 * See [prefers-color-scheme](https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme)
+	 * for details about OS-preferred color schemes.
+	 */
+	colorScheme?: 'light' | 'dark';
 }
 
 export class Config implements NDSConfig {
-	public namespace: NDSConfig['namespace'] = Config.defaults.namespace;
-
-	public static defaults: Required<NDSConfig> = {
+	public static readonly defaults: Required<NDSConfig> = {
 		namespace: 'nds',
+		colorScheme: 'light',
 	}
 
-	public configure(config: Partial<NDSConfig>): this {
-		if (config.namespace !== undefined) {
-			this.namespace = config.namespace;
-		}
+	public namespace: string | null = Config.defaults.namespace;
+	public colorScheme: 'light' | 'dark' = Config.defaults.colorScheme;
 
-		return this;
+	public constructor() {
+		localForage.config({ name: this.namespace as string });
 	}
 
-	public reset(): void {
-		Object.assign(this, Config.defaults);
+	public prefix = (val: string, delimiter = '-'): string => {
+		if (!this.namespace) return val;
+		return this.namespace + delimiter + val;
 	}
 }
 
+// Global configuration
 export const config = new Config();
-export const configure = config.configure.bind(config);
-export const reset = config.reset.bind(config);
+
+export const { prefix } = config;
+
+export const configure = (conf: NDSConfig): NDSConfig => Object.assign(config, conf);
+
+export const reset = (): NDSConfig => Object.assign(config, Config.defaults);
