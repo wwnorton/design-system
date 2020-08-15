9172070dead69f3d9d6308ebaf723805f2071d2a
Evan Yamanishi <yamanishi1@gmail.com>
2020-08-15 14:35:07 -0400

    refactor(storybook): update button stories for v6

--- packages/react/src/components/Button/index.stories.tsx
+++ packages/react/src/components/Button/index.stories.tsx
@@ -1,96 +1,97 @@
 import React from 'react';
-import {
-	withKnobs, select, boolean, text,
-} from '@storybook/addon-knobs';
-import { IconVariant, SVGIcon, IconOptions } from '../Icon';
+import { Story } from '@storybook/react';
+import { Button, IconButton, ButtonProps } from '.';
+import { IconButtonProps } from './IconButton';
+import { IconOptions } from '../Icon';
 import { prefix } from '../../config';
-import {
-	Button, ButtonVariant, IconButton, ButtonProps,
-} from '.';
+
+const baseName = prefix('button');
 
 export default {
 	title: 'Button',
 	component: Button,
-	decorators: [withKnobs],
+	subcomponents: { IconButton },
+	argTypes: {
+		variant: {
+			control: {
+				type: 'inline-radio',
+				options: {
+					None: undefined,
+					Solid: 'solid',
+					Outline: 'outline',
+					Ghost: 'ghost',
+				},
+			},
+		},
+		icon: {
+			control: {
+				type: 'select',
+				options: { None: undefined, ...IconOptions },
+			},
+		},
+		className: {
+			name: 'color modifier (className)',
+			control: {
+				type: 'select',
+				options: {
+					None: undefined,
+					Blue: `${baseName}--blue`,
+					Cyan: `${baseName}--cyan`,
+					Gray: `${baseName}--gray`,
+					Green: `${baseName}--green`,
+					Navy: `${baseName}--navy`,
+					Purple: `${baseName}--purple`,
+					Red: `${baseName}--red`,
+					Teal: `${baseName}--teal`,
+					Yellow: `${baseName}--yellow`,
+				},
+			},
+		},
+	},
 };
 
-const variantOptions: Record<string, ButtonVariant | undefined> = {
-	Solid: 'solid',
-	Outline: 'outline',
-	Ghost: 'ghost',
-	None: undefined,
-};
+const ButtonTemplate: Story<ButtonProps> = (args) => <Button {...args} />;
 
-const baseName = prefix('button');
+export const Solid = ButtonTemplate.bind({});
+Solid.args = { variant: 'solid', children: 'Solid' };
+export const Outline = ButtonTemplate.bind({});
+Outline.args = { variant: 'outline', children: 'Outline' };
+export const Ghost = ButtonTemplate.bind({});
+Ghost.args = { variant: 'ghost', children: 'Ghost' };
 
-const colorOptions = {
-	Primary: undefined,
-	Blue: `${baseName}--blue`,
-	Cyan: `${baseName}--cyan`,
-	Gray: `${baseName}--gray`,
-	Green: `${baseName}--green`,
-	Navy: `${baseName}--navy`,
-	Purple: `${baseName}--purple`,
-	Red: `${baseName}--red`,
-	Teal: `${baseName}--teal`,
-	Yellow: `${baseName}--yellow`,
-};
-
-export const Default: React.FunctionComponent = () => (
-	<Button
-		className={select('Color family', colorOptions, undefined)}
-		variant={select<ButtonVariant | undefined>('Variant', variantOptions, 'solid')}
-		disabled={boolean('Disabled', false)}
-		icon={select<IconVariant | undefined>('Icon', { None: undefined, ...IconOptions }, undefined)}
-		iconOnly={boolean('Icon only', false)}
-		iconRight={boolean('Icon on the right', false)}
-	>
-		{ text('Text', 'Button') }
-	</Button>
-);
-
-export const iconButton: React.FunctionComponent = () => (
-	<IconButton
-		disabled={boolean('Disabled', false)}
-		icon={select<IconVariant>('Icon', IconOptions, 'close')}
-		tooltipProps={{ placement: 'right' }}
-	>
-		{ text('Text', 'Close') }
-	</IconButton>
-);
-
-const moonIcon: SVGIcon = {
-	d: 'M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.79 1.42-1.41zM4 10.5H1v2h3v-2zm9-9.95h-2V3.5h2V.55zm7.45 3.91l-1.41-1.41-1.79 1.79 1.41 1.41 1.79-1.79zm-3.21 13.7l1.79 1.8 1.41-1.41-1.8-1.79-1.4 1.4zM20 10.5v2h3v-2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm-1 16.95h2V19.5h-2v2.95zm-7.45-3.91l1.41 1.41 1.79-1.8-1.41-1.41-1.79 1.8z',
-	source: 'https://material.io/resources/icons/?icon=wb_sunny&style=baseline',
-};
-
-export const CustomIcon: React.FunctionComponent = () => (
-	<Button
-		variant={select<ButtonVariant | undefined>('Variant', variantOptions, 'solid')}
-		disabled={boolean('Disabled', false)}
-		icon={moonIcon}
-		iconOnly
-	>
-		{ text('Text', 'Light') }
-	</Button>
-);
-
-export const ChangingContent: React.FunctionComponent = () => {
+export const ChangingContent: Story<Omit<ButtonProps, 'children' | 'icon'>> = (args) => {
 	const [favorite, setFavorite] = React.useState(false);
 
 	const toggleFavorite = (): void => setFavorite(!favorite);
 
-	const buttonProps: ButtonProps = React.useMemo(() => {
+	const buttonProps: Pick<ButtonProps, 'children' | 'icon'> = React.useMemo(() => {
 		if (favorite) return { children: 'Unfavorite', icon: 'heart' };
 		return { children: 'Favorite', icon: 'heart-outline' };
 	}, [favorite]);
 
 	return (
 		<Button
-			variant="solid"
 			onClick={toggleFavorite}
-			iconOnly={boolean('Icon only', false)}
 			{...buttonProps}
+			{...args}
 		/>
 	);
 };
+ChangingContent.args = { variant: 'solid' };
+
+const IconButtonTemplate: Story<IconButtonProps> = (args) => <IconButton {...args} />;
+
+export const IconButtonStory = IconButtonTemplate.bind({});
+IconButtonStory.args = { icon: 'close', children: 'Close' };
+IconButtonStory.storyName = 'Icon Button (close)';
+
+export const CustomIcon = IconButtonTemplate.bind({});
+CustomIcon.args = {
+	children: 'Light',
+	icon: {
+		d: 'M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.79 1.42-1.41zM4 10.5H1v2h3v-2zm9-9.95h-2V3.5h2V.55zm7.45 3.91l-1.41-1.41-1.79 1.79 1.41 1.41 1.79-1.79zm-3.21 13.7l1.79 1.8 1.41-1.41-1.8-1.79-1.4 1.4zM20 10.5v2h3v-2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm-1 16.95h2V19.5h-2v2.95zm-7.45-3.91l1.41 1.41 1.79-1.8-1.41-1.41-1.79 1.8z',
+		source: 'https://material.io/resources/icons/?icon=wb_sunny&style=baseline',
+	},
+	variant: 'solid',
+};
+CustomIcon.storyName = 'Icon Button (custom)';
