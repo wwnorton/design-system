84a649ea34ebfe1ffc93b0b8f6c060fcee330fde
Evan Yamanishi <yamanishi1@gmail.com>
2020-08-15 18:33:34 -0400

    docs: update typescript config
    - replace react-docgen-typescript-loader with react-dogcen-typescript-plugin
    - add mdx to tsconfig-paths-plugin resolution
    - update tsconfig

--- website/plugin-typescript.js
+++ website/plugin-typescript.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const { default: ReactDocgenTypescriptPlugin } = require('react-docgen-typescript-plugin');
 
 module.exports = () => ({
 	name: 'plugin-typescript',
@@ -9,23 +10,25 @@ module.exports = () => ({
 				{
 					test: /\.tsx?$/,
 					include: path.resolve(__dirname, '../packages/react'),
-					use: [
-						require.resolve('ts-loader'),
-						{
-							loader: require.resolve('react-docgen-typescript-loader'),
-							options: {
-								tsconfigPath: path.resolve(__dirname, '../packages/react/tsconfig.json'),
-							},
-						},
-					],
+					loader: 'ts-loader',
 				},
 			],
 		},
+		plugins: [
+			new ReactDocgenTypescriptPlugin({
+				tsconfigPath: path.resolve(__dirname, '../packages/react/tsconfig.json'),
+				include: [path.join(__dirname, '../packages/react/src/**/*!(.stories|.test).tsx')],
+				propFilter: (prop) => {
+					if (prop.parent) {
+						return !prop.parent.fileName.includes('node_modules');
+					}
+					return true;
+				},
+			}),
+		],
 		resolve: {
-			extensions: ['.ts', '.tsx'],
-			plugins: [new TsconfigPathsPlugin({
-				configFile: path.resolve(__dirname, 'tsconfig.json'),
-			})],
+			extensions: ['.mdx', '.ts', '.tsx'],
+			plugins: [new TsconfigPathsPlugin()],
 		},
 	}),
 });
