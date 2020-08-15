ee7a6625582deef355da77221c7cfe19497ce186
Evan Yamanishi <yamanishi1@gmail.com>
2020-08-15 14:33:45 -0400

    chore(dev-deps): update storybook to v6

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,14 +1,10 @@
 const path = require('path');
 
-const tsConfig = path.resolve(__dirname, '../tsconfig.json');
-
 module.exports = {
 	stories: [path.resolve(__dirname, '../src/**/*.stories.{ts,tsx,mdx}')],
 	addons: [
 		'@storybook/addon-a11y',
-		'@storybook/addon-actions',
-		'@storybook/addon-knobs',
-		'@storybook/addon-storysource',
+		'@storybook/addon-essentials',
 		{
 			name: '@storybook/preset-scss',
 			options: {
@@ -17,27 +13,15 @@ module.exports = {
 				},
 			},
 		},
-		{
-			name: '@storybook/preset-typescript',
-			options: {
-				include: [path.resolve(__dirname, '../src')],
-				forkTsCheckerWebpackPluginOptions: {
-					tsconfig: tsConfig,
-				},
-				tsLoaderOptions: {
-					configFile: tsConfig,
-				},
-				tsDocgenLoaderOptions: {
-					// filter out HTML props
-					// https://github.com/styleguidist/react-docgen-typescript#parseroptions
-					propFilter(prop) {
-						if (prop.parent) {
-							return !prop.parent.fileName.includes('node_modules');
-						}
-						return true;
-					},
-				},
+	],
+	typescript: {
+		reactDocgen: 'react-docgen-typescript',
+		reactDocgenTypescriptOptions: {
+			shouldExtractLiteralValuesFromEnum: true,
+			propFilter: (prop) => {
+				if (prop.parent) return !/node_modules/.test(prop.parent.fileName);
+				return true;
 			},
 		},
-	],
+	}
 }
