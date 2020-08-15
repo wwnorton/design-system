b02d6e08234ba8989943e37ca81cf13f5e369297
Evan Yamanishi <yamanishi1@gmail.com>
2020-08-15 17:41:48 -0400

    refactor(react): use more readable `tagName` and avoid `is` HTML conflict

--- packages/react/src/components/BasePopper/index.tsx
+++ packages/react/src/components/BasePopper/index.tsx
@@ -5,11 +5,8 @@ import { useForwardedRef, usePopper } from '../../hooks';
 export interface BasePopperProps extends React.HTMLAttributes<HTMLElement>, Partial<Options> {
 	/** Indicates whether the popper is rendered or not. */
 	isOpen?: boolean;
-	/**
-	 * The outer HTML element name. Default is `div`.
-	 * @see https://html.spec.whatwg.org/multipage/custom-elements.html#attr-is
-	 */
-	is?: 'div' | 'section' | 'ul' | 'ol' | 'dl';
+	/** The outer HTML element name. Default is `div`. */
+	tagName?: 'div' | 'section' | 'ul' | 'ol' | 'dl';
 	/**
 	 * The reference element that the popper will be attached to.
 	 * @see https://popper.js.org/docs/v2/constructors/#createpopper
@@ -25,7 +22,7 @@ export interface BasePopperProps extends React.HTMLAttributes<HTMLElement>, Part
 export const BasePopper = React.forwardRef<HTMLElement, BasePopperProps>((
 	{
 		isOpen = false,
-		is: Tag = 'div',
+		tagName: Tag = 'div',
 		reference,
 		children,
 		placement = 'auto',
