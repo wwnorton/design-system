dcbed0a9346770d04882850bed6aafe6f62dca82
Evan Yamanishi <yamanishi1@gmail.com>
2020-08-15 18:34:11 -0400

    docs(theme): flesh out PropsTable component

---
+++ website/src/theme/PropsTable/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { FieldInfo } from '@wwnds/react';
+/* eslint-disable import/no-extraneous-dependencies */
+import { ComponentDoc, PropItem } from 'react-docgen-typescript';
+import Markdown from 'markdown-to-jsx';
+
+const PropRow = ({
+	name,
+	required,
+	type,
+	description,
+	// defaultValue,
+}: PropItem): JSX.Element => (
+	<tr>
+		<td><FieldInfo indicator={(required) ? 'required' : undefined} label={name} /></td>
+		<td><Markdown>{ description }</Markdown></td>
+		<td><code>{ type.name.replace(' | undefined', '') }</code></td>
+		{/* <td>
+			{ (defaultValue && <code>{ defaultValue.value }</code>) || '-' }
+		</td> */}
+	</tr>
+);
+
+type PropsTableProps = {
+	from: typeof React.Component & { __docgenInfo: ComponentDoc; };
+}
+
+export const PropsTable = ({ from: component }: PropsTableProps): JSX.Element | null => {
+	if (!('__docgenInfo' in component)) return null;
+	// eslint-disable-next-line no-underscore-dangle
+	const { displayName, props } = component.__docgenInfo;
+	const propMap = Object.keys(props).map((prop) => {
+		const propItem = props[prop];
+		return <PropRow key={propItem.name} {...propItem} />;
+	});
+	return (
+		<table aria-label={`Props for ${displayName}`}>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Description</th>
+					<th>Type</th>
+					{/* <th>Default Value</th> */}
+				</tr>
+			</thead>
+			<tbody>{ propMap }</tbody>
+		</table>
+	);
+};
