a967eaa4d8044696b4c0c2536ef71c1216dfa145
Evan Yamanishi <yamanishi1@gmail.com>
2020-09-25 15:14:15 -0400

    chore: update stylelint and restore formerly-broken rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -13,12 +13,6 @@ module.exports = {
 		'scss/at-function-pattern': '^[_-]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
 		'scss/at-mixin-pattern': '^[_-]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
 		'scss/dollar-variable-pattern': '^[_-]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
-		/**
-		 * temporarily disable *-empty-lin-before rules
-		 * See https://github.com/stylelint/stylelint/issues/4917
-		 */
-		'at-rule-empty-line-before': null,
-		'declaration-empty-line-before': null,
 	},
 	ignoreFiles: ['**/*.jsx', '**/*.tsx'],
 };
