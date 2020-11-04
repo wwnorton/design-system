6b48fff22aaa46863e0cd4c8aeee564c94c9c26a
Evan Yamanishi <yamanishi1@gmail.com>
2020-11-04 14:58:55 -0500

    feat(react): make it easier to disble TextField's counter

--- packages/react/src/components/TextField/index.tsx
+++ packages/react/src/components/TextField/index.tsx
@@ -31,10 +31,10 @@ export interface TextFieldProps
 	 * number of characters and returns the string or element that will be
 	 * rendered in the character counter slot.
 	 */
-	counter?: ({ remaining, max }: {
+	counter?: false | (({ remaining, max }: {
 		remaining: number;
 		max: number;
-	}) => React.ReactNode;
+	}) => React.ReactNode);
 	/** The base class name according to BEM conventions. */
 	baseName?: string;
 	/** The className for the TextField's `<input>` element. */
