f3441cab8b7e0b3326dd42d82911af27110f5b5f
Evan Yamanishi <yamanishi1@gmail.com>
2020-11-23 15:53:30 -0500

    feat(react): add custom email validation
    Addresses #43.

--- packages/react/src/utilities/validation.ts
+++ packages/react/src/utilities/validation.ts
@@ -73,6 +73,14 @@ export type ValidationAttributes =
 
 type StateMessageFunction = (attrs: ValidationAttributes, value?: string) => string;
 
+/* eslint-disable no-control-regex */
+/**
+ * A custom regex for email.
+ * Source: [Stack Overflow](https://stackoverflow.com/a/201378).
+ */
+export const emailTypeMismatch = (value: string): boolean => /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(value);
+/* eslint-enable no-control-regex */
+
 /** Message functions that correspond to each invalid key on ValidityState. */
 export const defaultMessages: Record<Exclude<ValidityStateInvalidKeys, 'customError'>, StateMessageFunction> = {
 	/**
@@ -147,7 +155,7 @@ export const defaultValidators = ({
 	if (type !== undefined) {
 		validators.push({
 			name: 'typeMismatch',
-			test: validityStateTest('typeMismatch'),
+			test: (type === 'email') ? emailTypeMismatch : validityStateTest('typeMismatch'),
 			message: defaultMessages.typeMismatch({ type }),
 		});
 		validators.push({
