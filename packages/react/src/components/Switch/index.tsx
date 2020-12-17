952250ca593f933aa5d4e6fa6a72f6a73aece7e6
Evan Yamanishi <yamanishi1@gmail.com>
2020-12-16 23:40:30 -0500

    chore: use relative lodash packages
    this prevents the entirety of lodash being bundled.

--- packages/react/src/components/Switch/index.tsx
+++ packages/react/src/components/Switch/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import { uniqueId } from 'lodash';
+import uniqueId from 'lodash/uniqueId';
 import { Button, ButtonProps } from '../Button';
 import { FieldInfo, FieldInfoCoreProps } from '../Field';
 import { Tooltip, TooltipCoreProps } from '../Tooltip';
--- packages/react/src/components/Tooltip/index.tsx
+++ packages/react/src/components/Tooltip/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import { uniqueId } from 'lodash';
+import uniqueId from 'lodash/uniqueId';
 import { BasePopper, BasePopperProps } from '../BasePopper';
 import { prefix } from '../../config';
 import { innerText } from '../../utilities';
