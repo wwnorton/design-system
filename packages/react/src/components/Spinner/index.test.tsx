c019494c60c0b037f557c4d60e13a91b159781a7
Apatil15 <apatil@wwnorton.com>
2020-12-22 22:56:49 -0500

    feat: update basic unit test cases

--- packages/react/src/components/Spinner/index.test.tsx
+++ packages/react/src/components/Spinner/index.test.tsx
@@ -3,23 +3,23 @@ import React from 'react';
 import {
 	cleanup, render, screen,
 } from '@testing-library/react';
-import { ProgressIndicator } from '.';
+import { Spinner } from '.';
 
 test.afterEach(cleanup);
 
 test('renders variant determinate with progress value 25 ', (t) => {
 	const progressValue = 25;
-	render(<ProgressIndicator variant="determinate" progressValue={progressValue} />);
-	t.is(screen.getByLabelText('progressbar'), screen.getByRole('status'));
+	render(<Spinner variant="determinate" value={progressValue} />);
+	t.truthy(screen.getByRole('status'));
 });
 
 test('renders progress indicator color change', (t) => {
-	render(<ProgressIndicator color="primary" />);
-	t.is(screen.getByLabelText('progressbar'), screen.getByRole('status'));
+	render(<Spinner color="red" />);
+	t.truthy(screen.getByRole('status'));
 });
 
 test('renders progress indicator with label', (t) => {
 	const labelText = 'loading images...';
-	render(<ProgressIndicator color="primary" label={labelText} />);
+	render(<Spinner color="primary" label={labelText} />);
 	t.truthy(screen.getByText(labelText));
 });
