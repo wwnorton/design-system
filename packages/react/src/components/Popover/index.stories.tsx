5898b0a48983c72c670c3b2722454809e640fa7b
Evan Yamanishi <yamanishi1@gmail.com>
2021-02-20 22:14:44 -0500

    docs(popover): simplify stories

--- packages/react/src/components/Popover/index.stories.tsx
+++ packages/react/src/components/Popover/index.stories.tsx
@@ -6,9 +6,7 @@ import {
 	text,
 } from '@storybook/addon-knobs';
 import { placements } from '@popperjs/core/lib/enums';
-import { Popover } from '.';
-import { Button } from '../Button';
-import { TextField } from '../TextField';
+import { Popover, Button, TextField } from '../..';
 
 export default {
 	title: 'Popover',
@@ -19,22 +17,34 @@ export default {
 	},
 };
 
-export const Default: React.FunctionComponent = () => (
-	<Popover
-		title={text('Title', 'Popover title')}
-		hideTitle={boolean('Hide title', false)}
-		hideCloseButton={boolean('Hide close button', false)}
-		placement={select('Placement', placements, 'bottom-start')}
-		isOpen
-	>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-		Ut enim ad minim veniam, quis nostrud exercitation ullamco
-	</Popover>
-);
+export const Default: React.FunctionComponent = () => {
+	const [isOpen, setIsOpen] = React.useState(true);
+	return (
+		<Popover
+			title={text('Title', 'Popover title')}
+			hideTitle={boolean('Hide title', false)}
+			hideCloseButton={boolean('Hide close button', false)}
+			placement={select('Placement', placements, 'bottom-start')}
+			isOpen={isOpen}
+			onRequestClose={() => setIsOpen(false)}
+		>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		</Popover>
+	);
+};
 
-export const WithReference: React.FunctionComponent = () => {
-	const [ref, setRef] = React.useState<HTMLButtonElement | null>(null);
+/**
+ * This example demos the minimal setup.
+ * - The triggering element must use a callback ref, which is bound to the
+ *   Popover's `reference` prop. This lets the popover know where to position.
+ * - Requests to open and close are used to update the `isOpen` state.
+ */
+export const Minimal: React.FunctionComponent = () => {
+	const [isOpen, setIsOpen] = React.useState(false);
+	const [ref, setRef] = React.useState<HTMLButtonElement | null>();
+
 	return (
 		<>
 			<Button variant="solid" ref={setRef}>
@@ -42,10 +52,13 @@ export const WithReference: React.FunctionComponent = () => {
 			</Button>
 			<Popover
 				title={text('Title', 'Popover title')}
-				hideTitle={boolean('Hide title', false)}
-				hideCloseButton={boolean('Hide close button', false)}
+				hideTitle={boolean('Hide title', true)}
+				hideCloseButton={boolean('Hide close button', true)}
 				placement={select('Placement', placements, 'bottom-start')}
 				reference={ref}
+				isOpen={isOpen}
+				onRequestClose={() => setIsOpen(false)}
+				onRequestOpen={() => setIsOpen(true)}
 			>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit,
 				sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -58,9 +71,11 @@ export const WithReference: React.FunctionComponent = () => {
 /**
  * This example demos how to fully control the state of a Popover.
  * The following features are related to the Popover API:
- * - use of `isOpen` forces the popover into controlled (manual) state. this means
- *   that the story is responsible for implementing all actions that open/close the popover.
- * - popover is labelled by the text field's label, so it doesn't need a `title`
+ * - The Popover is labelled by the text field's label, so it doesn't need a `title`.
+ * - The input is focused on open, overriding the default `onOpen` callback, which
+ *   focuses the popover dialog itself.
+ * - Opening is triggered by setting an `onClick` callback on the reference button
+ *   so `onRequestOpen` is not used.
  *
  * The following features are unrelated to Popover but are good practice:
  * - the submit button ([type=submit]) is external to the form so it uses the `form`
@@ -69,26 +84,32 @@ export const WithReference: React.FunctionComponent = () => {
  *   onClick callback.
  * - a text field contained by a form will trigger the onSubmit action on Enter
  */
-export const Controlled: React.FunctionComponent = () => {
+export const FullyControlled: React.FunctionComponent = () => {
 	const [ref, setRef] = React.useState<HTMLButtonElement | null>();
 	const [buttonText, setButtonText] = React.useState('Change this button\'s text');
 	const [inputText, setInputText] = React.useState(buttonText);
 	const [input, setInput] = React.useState<HTMLInputElement | null>(null);
 	const [isOpen, setOpen] = React.useState(false);
 
-	const toggle = (): void => {
+	const open = (): void => {
 		setInputText(buttonText);
-		setOpen(!isOpen);
+		setOpen(true);
 	};
-	const close = () => setOpen(false);
+	const close = (trigger) => {
+		// don't close on reference clicks since we're using our own onClick callback
+		if (trigger !== 'click.reference') setOpen(false);
+	};
+	const toggle = (t) => ((isOpen) ? close(t) : open());
+
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
 		if (input) setInputText(e.target.value);
 	};
 	const submit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		setButtonText(inputText);
-		close();
+		close('submit');
 	};
+
 	return (
 		<>
 			<Button variant="solid" ref={setRef} onClick={toggle}>
@@ -97,10 +118,11 @@ export const Controlled: React.FunctionComponent = () => {
 			<Popover
 				aria-labelledby="change-btn"
 				hideCloseButton={boolean('Hide close button', true)}
-				placement={select('Placement', placements, 'top-start')}
+				placement={select('Placement', placements, 'bottom-start')}
 				reference={ref}
 				isOpen={isOpen}
 				onRequestClose={close}
+				onOpen={() => input && input.focus()}
 				actions={boolean('Action bar', true) && (
 					<>
 						<Button variant="outline" onClick={close}>
