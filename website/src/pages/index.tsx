dd96d3cc1d3b77a431b421425a41924b2726d81e
Evan Yamanishi <sh0ji@users.noreply.github.com>
2021-02-21 02:01:37 -0500

    Tests fix (#88)
    * chore(test): increase timeout to 1m
    
    * chore: always run tests in verbose when reporting
    
    * chore: remove unused vars
    
    * chore: add yarn to engines
    
    * chore: remove unused utilities

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import clsx from 'clsx';
 import classNames from 'classnames';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
