e6558dbc4533e2feff79973c5696f260a4c185dc
Evan Yamanishi <yamanishi1@gmail.com>
2021-02-21 01:49:39 -0500

    chore: remove unused utilities

--- packages/react/src/utilities/helpers.ts
+++ packages/react/src/utilities/helpers.ts
@@ -1,38 +1,5 @@
 import React from 'react';
 
-/** Test if an element is hidden. By default, this includes `aria-hidden`. */
-export const isHidden = (el: React.ReactElement, ariaHidden = true): boolean => {
-	if (ariaHidden && (
-		el.props['aria-hidden'] === true || el.props['aria-hidden'] === 'true'
-	)) return true;
-	if ('style' in el.props) {
-		const { display, visibility } = el.props.style;
-		if (visibility && visibility !== 'visible') return true;
-		if (display === 'none') return true;
-	}
-	if (el.props.hidden) return true;
-	return false;
-};
-
-/**
- * Recursively get the visible text content of an element and its descendants,
- * similar to Node.innerText.
- */
-export const innerText = (children: React.ReactNode): string => {
-	const strings = React.Children.map(children, (child) => {
-		if (React.isValidElement(child)) {
-			if (isHidden(child)) return '';
-			return innerText(child.props.children);
-		}
-		if (Array.isArray(child)) return innerText(child);
-		if (typeof child === 'number') return child.toString();
-		if (typeof child === 'string') return child;
-		return null;
-	});
-	if (strings) return strings.filter(Boolean).join('');
-	return '';
-};
-
 export const focusableSelectors = [
 	'[contentEditable=true]:not([tabindex="-1"])',
 	'[tabindex]:not([tabindex="-1"])',
