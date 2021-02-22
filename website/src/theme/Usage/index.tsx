a5dbdac7670b38ef5724cab364306aca854dc248
Evan Yamanishi <yamanishi1@gmail.com>
2021-02-22 02:03:23 -0500

    docs: add experimental usage dos/don'ts component

---
+++ website/src/theme/Usage/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { CalloutError, CalloutSuccess } from '@wwnds/react';
+import styles from './styles.module.scss';
+
+interface UsageProps {
+	good: string;
+	bad: string;
+	children: React.ReactNode;
+}
+
+export const Usage: React.FunctionComponent<UsageProps> = ({ good, bad, children }: UsageProps) => (
+	<section className={styles.usage}>
+		<CalloutError
+			title="Don't"
+			border="bottom"
+			tag="div"
+			className={styles.usage__bad}
+		>
+			{ bad }
+		</CalloutError>
+		<CalloutSuccess
+			title="Do"
+			border="bottom"
+			tag="div"
+			className={styles.usage__good}
+		>
+			{ good }
+		</CalloutSuccess>
+		<p className={styles.usage__why}>
+			<strong>Why</strong>
+			{ ': ' }
+			{ React.Children.map(children, (child) => {
+				if (React.isValidElement(child)) return child.props.children;
+				return child;
+			}) }
+		</p>
+	</section>
+);
