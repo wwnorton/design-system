b2b942a32720460c9b188d091c61cfeb90b51fa1
Evan Yamanishi <yamanishi1@gmail.com>
2021-02-22 11:21:54 -0500

    refactor(core): allow mixins anywhere in the order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -13,6 +13,18 @@ module.exports = {
 		'scss/at-function-pattern': '^[_-]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
 		'scss/at-mixin-pattern': '^[_-]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
 		'scss/dollar-variable-pattern': '^[_-]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+		'order/order': [
+			[
+				'custom-properties',
+				'dollar-variables',
+				{
+					type: 'at-rule',
+					name: 'extend',
+				},
+				'declarations',
+				'rules',
+			],
+		],
 	},
 	ignoreFiles: ['**/*.jsx', '**/*.tsx'],
 };
