645bdd676d837e81b95bd3b9aa5a99aebd3ce704
Evan Yamanishi <yamanishi1@gmail.com>
2021-02-22 15:19:45 -0500

    chore: remove focus-visible polyfill now that it's supported in browsers

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -3,7 +3,3 @@ import './globals.scss';
 export const parameters = {
 	controls: { expanded: true },
 };
-
-const focusVisible = document.createElement('script');
-focusVisible.setAttribute('src', 'https://unpkg.com/focus-visible');
-document.head.appendChild(focusVisible);
