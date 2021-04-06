d1b7cd36943b45ec7827231817ba276fc118087b
Evan Yamanishi <yamanishi1@gmail.com>
2021-04-06 00:24:11 -0400

    test: add mediaQuery tests & tools
    matchMedia is not supported by JSDOM so we
    have to polyfill it with mq-polyfill.

---
+++ packages/react/src/utilities/mediaQuery/index.test.tsx
@@ -0,0 +1,30 @@
+import test from 'ava';
+import { renderHook } from '@testing-library/react-hooks';
+import matchMediaPolyfill from 'mq-polyfill';
+import { useMediaQuery } from './hook';
+
+test.before(() => {
+	matchMediaPolyfill(window);
+	window.resizeTo = function resizeTo(width, height) {
+		Object.assign(this, {
+			innerWidth: width,
+			innerHeight: height,
+			outerWidth: width,
+			outerHeight: height,
+		}).dispatchEvent(new this.Event('resize'));
+	};
+	window.resizeTo(400, 400);
+});
+
+test('should be false when the media query doesn\'t match', (t) => {
+	const { result } = renderHook(() => useMediaQuery('(min-width: 600px)'));
+
+	t.false(result.current);
+});
+
+test('should be true when the conditions of the query match', (t) => {
+	window.resizeTo(800, 800);
+	const { result } = renderHook(() => useMediaQuery('(min-width: 600px)'));
+
+	t.true(result.current);
+});
