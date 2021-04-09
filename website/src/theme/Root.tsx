596cc455eebd2f39404148d01f9314f63de974c9
Evan Yamanishi <yamanishi1@gmail.com>
2021-04-09 19:20:56 -0400

    docs(refactor): use AppProvider for all color scheme needs

--- website/src/theme/Root.tsx
+++ website/src/theme/Root.tsx
@@ -3,27 +3,35 @@ import { canUseDOM, ColorScheme, AppProvider } from '@wwnds/react';
 import Head from '@docusaurus/Head';
 import Link from '@docusaurus/Link';
 
+const COLOR_SCHEME_KEY = 'nds-color-scheme';
+
 const Root: React.FunctionComponent = ({ children }: { children?: React.ReactNode }) => {
+	// get the initial color scheme from local storage
 	const colorScheme = React.useMemo(() => {
 		if (canUseDOM && 'localStorage' in window) {
-			const storedScheme = window.localStorage.getItem('nds-color-scheme');
+			const storedScheme = window.localStorage.getItem(COLOR_SCHEME_KEY);
 			if (storedScheme) return storedScheme as ColorScheme;
 		}
 		return undefined;
 	}, []);
 
-	React.useEffect(() => {
-		if ('indexedDB' in window) {
-			window.indexedDB.deleteDatabase('nds');
+	// update local storage any time the color scheme changes
+	const storeScheme = (scheme: ColorScheme): void => {
+		if ('localStorage' in window) {
+			window.localStorage.setItem(COLOR_SCHEME_KEY, scheme);
 		}
-	}, []);
+	};
 
 	return (
 		<>
 			<Head>
 				<link rel="stylesheet" href="https://use.typekit.net/aoc6aci.css" />
 			</Head>
-			<AppProvider linkComponent={Link} colorScheme={colorScheme}>
+			<AppProvider
+				linkComponent={Link}
+				colorScheme={colorScheme}
+				onColorSchemeChange={storeScheme}
+			>
 				{ children }
 			</AppProvider>
 		</>
