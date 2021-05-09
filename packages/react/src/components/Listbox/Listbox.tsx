d0655e2066986dd0e33ac91843961d738d5b6148
Evan Yamanishi <yamanishi1@gmail.com>
2021-05-09 13:20:14 -0400

    perf(listbox): memoize the options map

--- packages/react/src/components/Listbox/Listbox.tsx
+++ packages/react/src/components/Listbox/Listbox.tsx
@@ -104,6 +104,109 @@ export const Listbox = React.forwardRef<HTMLUListElement, ListboxProps>(({
 		...keys,
 	});
 
+	const Options = React.useMemo(() => (
+		options.map(({ props }, i) => {
+			const mappedProps = (typeof optionProps === 'function')
+				? optionProps(i)
+				: optionProps;
+
+			const {
+				children,
+				disabled,
+				value,
+				label = children || value,
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				selected: optionSelected,
+				...optProps
+			}: OptionProps = {
+				...mappedProps,
+				...props,
+			};
+
+			if (!value) {
+				throw new Error('<Option> elements must provide a value prop.');
+			}
+
+			if (disabled) {
+				disabledOptions.current.add(i);
+			} else {
+				disabledOptions.current.delete(i);
+			}
+
+			const select = () => {
+				if (onChange) {
+					onChange({
+						children,
+						disabled,
+						...optProps,
+						value,
+						label,
+					});
+				} else if (!selectedProp) {
+					toggle(value);
+				}
+			};
+
+			return (
+				<Option
+					key={value}
+					value={value}
+					label={label}
+					selected={selected.includes(value)}
+					disabled={disabled}
+					tabIndex={(disabled) ? -1 : tabIndex(i)}
+					/**
+					 * - do nothing if disabled
+					 * - move focus to the index
+					 * - select the option
+					 */
+					onClick={(e) => {
+						if (disabled) {
+							e.preventDefault();
+							return;
+						}
+						dispatch({ type: 'GOTO', payload: i });
+						select();
+					}}
+					/**
+					 * - select on enter
+					 * - begin click on space bar
+					 */
+					onKeyDown={(e) => {
+						if (e.key === 'Enter') select();
+						if (e.key === ' ') {
+							e.preventDefault();
+							setKeyboardClick(true);
+						}
+					}}
+					/**
+					 * select on space bar up, but only if the space bar
+					 * was depressed on this option. this ensures that
+					 * clicks with space bar can be cancelled.
+					 */
+					onKeyUp={({ key }) => {
+						if (key === ' ' && keyboardClick) {
+							select();
+							setKeyboardClick(false);
+						}
+					}}
+					/** cancel space bar click */
+					onBlur={() => setKeyboardClick(false)}
+					onFocus={(e) => {
+						if (onOptionFocus) onOptionFocus(e, i);
+					}}
+					ref={(disabled) ? undefined : createRef(i)}
+					{...optProps}
+				>
+					{ children }
+				</Option>
+			);
+		})
+	), [
+		createRef, dispatch, keyboardClick, onChange, onOptionFocus,
+		optionProps, options, selected, selectedProp, tabIndex, toggle,
+	]);
+
 	return (
 		<ul
 			ref={setListbox}
@@ -112,108 +215,11 @@ export const Listbox = React.forwardRef<HTMLUListElement, ListboxProps>(({
 			onKeyDown={onKeyDown}
 			onBlur={onBlur}
 			{...listboxProps}
+			// role and aria-multiselectable cannot be overridden
 			role="listbox"
 			aria-multiselectable={multiselectable}
 		>
-			{
-				options.map(({ props }, i) => {
-					const mappedProps = (typeof optionProps === 'function')
-						? optionProps(i)
-						: optionProps;
-
-					const {
-						children,
-						disabled,
-						value,
-						label = children || value,
-						// eslint-disable-next-line @typescript-eslint/no-unused-vars
-						selected: optionSelected,
-						...optProps
-					}: OptionProps = {
-						...mappedProps,
-						...props,
-					};
-
-					if (!value) {
-						throw new Error('<Option> elements must provide a value prop.');
-					}
-
-					if (disabled) {
-						disabledOptions.current.add(i);
-					} else {
-						disabledOptions.current.delete(i);
-					}
-
-					const select = () => {
-						if (onChange) {
-							onChange({
-								children,
-								disabled,
-								...optProps,
-								value,
-								label,
-							});
-						} else if (!selectedProp) {
-							toggle(value);
-						}
-					};
-
-					return (
-						<Option
-							key={value}
-							value={value}
-							label={label}
-							selected={selected.includes(value)}
-							disabled={disabled}
-							tabIndex={(disabled) ? -1 : tabIndex(i)}
-							/**
-							 * - do nothing if disabled
-							 * - move focus to the index
-							 * - select the option
-							 */
-							onClick={(e) => {
-								if (disabled) {
-									e.preventDefault();
-									return;
-								}
-								dispatch({ type: 'GOTO', payload: i });
-								select();
-							}}
-							/**
-							 * - select on enter
-							 * - begin click on space bar
-							 */
-							onKeyDown={(e) => {
-								if (e.key === 'Enter') select();
-								if (e.key === ' ') {
-									e.preventDefault();
-									setKeyboardClick(true);
-								}
-							}}
-							/**
-							 * select on space bar up, but only if the space bar
-							 * was depressed on this option. this ensures that
-							 * clicks with space bar can be cancelled.
-							 */
-							onKeyUp={({ key }) => {
-								if (key === ' ' && keyboardClick) {
-									select();
-									setKeyboardClick(false);
-								}
-							}}
-							/** cancel space bar click */
-							onBlur={() => setKeyboardClick(false)}
-							onFocus={(e) => {
-								if (onOptionFocus) onOptionFocus(e, i);
-							}}
-							ref={(disabled) ? undefined : createRef(i)}
-							{...optProps}
-						>
-							{ children }
-						</Option>
-					);
-				})
-			}
+			{ Options }
 		</ul>
 	);
 });
