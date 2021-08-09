df697849fb3f50620bce9ad00b6d34909c03819f
Wilmer Guevara <wguevara@wwnorton.com>
2021-08-09 07:50:50 -0500

    feat(textarea): textfield indentation fixed

--- packages/react/src/components/TextField/index.tsx
+++ packages/react/src/components/TextField/index.tsx
@@ -15,208 +15,189 @@ const defaultProps: Partial<TextFieldProps> = {
 	type: 'text',
 };
 
-export const TextField = React.forwardRef<HTMLInputElement & HTMLTextAreaElement, TextFieldProps>(
-	(
-		{
-			// options
-			counterStart = defaultProps.counterStart,
-			validators,
-			validateOnChange,
-			validateOnDOMChange,
-			requiredIndicator,
-			optionalIndicator,
-			multiline = false,
-			autoSize = false,
+export const TextField = React.forwardRef<HTMLInputElement & HTMLTextAreaElement, TextFieldProps>((
+	{
+		// options
+		counterStart = defaultProps.counterStart,
+		validators,
+		validateOnChange,
+		validateOnDOMChange,
+		requiredIndicator,
+		optionalIndicator,
+		multiline = false,
+		autoSize = false,
 
-			// anatomy
-			children,
-			description,
-			addonBefore,
-			addonAfter,
-			feedback,
-			errors: errorsProp,
-			counter = defaultProps.counter,
+		// anatomy
+		children,
+		description,
+		addonBefore,
+		addonAfter,
+		feedback,
+		errors: errorsProp,
+		counter = defaultProps.counter,
 
-			// classes
-			baseName = 'nds-field',
-			className = classNames(baseName, `${baseName}--text`),
-			labelClass,
-			descriptionClass,
-			groupClass = classNames(`${baseName}__group`, `${baseName}__group--text`),
-			inputClass = classNames(`${baseName}__input`, `${baseName}__input--text`),
-			addonClass = `${baseName}__addon`,
-			feedbackClass,
-			errorsClass,
-			counterClass = `${baseName}__counter`,
-			invalidClass = `${baseName}--invalid`,
+		// classes
+		baseName = 'nds-field',
+		className = classNames(baseName, `${baseName}--text`),
+		labelClass,
+		descriptionClass,
+		groupClass = classNames(`${baseName}__group`, `${baseName}__group--text`),
+		inputClass = classNames(`${baseName}__input`, `${baseName}__input--text`),
+		addonClass = `${baseName}__addon`,
+		feedbackClass,
+		errorsClass,
+		counterClass = `${baseName}__counter`,
+		invalidClass = `${baseName}--invalid`,
 
-			// ids
-			id: idProp,
-			labelId: labelIdProp,
-			descriptionId: descIdProp,
-			errorsId: errIdProp,
+		// ids
+		id: idProp,
+		labelId: labelIdProp,
+		descriptionId: descIdProp,
+		errorsId: errIdProp,
 
-			// <input> attributes
-			maxLength,
-			required,
-			type = defaultProps.type,
-			value,
+		// <input> attributes
+		maxLength,
+		required,
+		type = defaultProps.type,
+		value,
 
-			// event callbacks
-			onChange,
-			onCount,
-			onDOMChange,
-			onValidate,
+		// event callbacks
+		onChange,
+		onCount,
+		onDOMChange,
+		onValidate,
 
-			// everything else
-			...inputProps
-		}: TextFieldProps,
-		ref,
-	) => {
-		const [errors, setErrors] = React.useState(errorsProp);
-		// ids stored as refs since they shouldn't change between renders
-		const id = React.useRef(idProp || uniqueId(`${baseName}-`));
-		const labelId = React.useRef(labelIdProp || `${id.current}-label`);
-		const descId = React.useRef(descIdProp || `${id.current}-desc`);
-		const errId = React.useRef(errIdProp || `${id.current}-err`);
-		const inputId = React.useRef(`${id.current}-input`);
+		// everything else
+		...inputProps
+	}: TextFieldProps, ref,
+) => {
+	const [errors, setErrors] = React.useState(errorsProp);
 
-		const getRemaining = React.useCallback(
-			(val?: typeof value) => {
-				if (maxLength) {
-					return maxLength - (val || '').toString().length;
-				}
-				return undefined;
-			},
-			[maxLength],
-		);
-		const [remaining, setRemaining] = React.useState(getRemaining(value));
-		React.useEffect(() => setRemaining(getRemaining(value)), [
-			getRemaining,
-			value,
-		]);
+	// ids stored as refs since they shouldn't change between renders
+	const id = React.useRef(idProp || uniqueId(`${baseName}-`));
+	const labelId = React.useRef(labelIdProp || `${id.current}-label`);
+	const descId = React.useRef(descIdProp || `${id.current}-desc`);
+	const errId = React.useRef(errIdProp || `${id.current}-err`);
+	const inputId = React.useRef(`${id.current}-input`);
 
-		// treat prop version of errors as source of truth
-		React.useEffect(() => setErrors(errorsProp), [errorsProp]);
+	const getRemaining = React.useCallback((val?: typeof value) => {
+		if (maxLength) {
+			return maxLength - (val || '').toString().length;
+		}
+		return undefined;
+	}, [maxLength]);
+	const [remaining, setRemaining] = React.useState(getRemaining(value));
+	React.useEffect(() => setRemaining(getRemaining(value)), [getRemaining, value]);
 
-		React.useEffect(() => {
-			if (onCount) onCount(remaining);
-		}, [onCount, remaining]);
+	// treat prop version of errors as source of truth
+	React.useEffect(() => setErrors(errorsProp), [errorsProp]);
 
-		const isValid = React.useMemo(
-			() => Boolean(!errors || errors.length === 0),
-			[errors],
-		);
+	React.useEffect(() => {
+		if (onCount) onCount(remaining);
+	}, [onCount, remaining]);
 
-		const validateHandler = (e: string[]): void => {
-			if (onValidate) onValidate(e);
-			setErrors(e);
-		};
+	const isValid = React.useMemo(() => Boolean(!errors || errors.length === 0), [errors]);
 
-		const changeHandler = (
-			e: React.ChangeEvent<HTMLInputElement>
-			& React.ChangeEvent<HTMLTextAreaElement>,
-		) => {
-			if (onChange) onChange(e);
-			else setRemaining(getRemaining(e.target.value));
-		};
+	const validateHandler = (e: string[]): void => {
+		if (onValidate) onValidate(e);
+		setErrors(e);
+	};
 
-		const createFieldAddons = (
-			addons: React.ReactNode,
-		): React.ReactNode[] | null | undefined => {
-			if (!addons) return null;
-			return React.Children.map(addons, (child) => {
-				if (React.isValidElement(child)) {
-					if (child.type === React.Fragment) {
-						return createFieldAddons(child.props.children);
-					}
-				}
-				return <FieldAddon className={addonClass}>{child}</FieldAddon>;
-			});
-		};
+	const changeHandler = (e: React.ChangeEvent<HTMLInputElement>
+	& React.ChangeEvent<HTMLTextAreaElement>) => {
+		if (onChange) onChange(e);
+		else setRemaining(getRemaining(e.target.value));
+	};
 
-		const Counter = React.useMemo(() => {
-			if (!counter) return null;
-			if (
-				maxLength !== undefined
-				&& remaining !== undefined
-				&& counterStart !== undefined
-			) {
-				if (remaining <= counterStart) {
-					return (
-						<div className={counterClass}>
-							{counter({ remaining, max: maxLength })}
-						</div>
-					);
+	const createFieldAddons = (addons: React.ReactNode): React.ReactNode[] | null | undefined => {
+		if (!addons) return null;
+		return React.Children.map(addons, (child) => {
+			if (React.isValidElement(child)) {
+				if (child.type === React.Fragment) {
+					return createFieldAddons(child.props.children);
 				}
 			}
-			return null;
-		}, [counter, counterClass, counterStart, maxLength, remaining]);
+			return <FieldAddon className={addonClass}>{ child }</FieldAddon>;
+		});
+	};
 
-		const indicator = React.useMemo(() => {
-			if (requiredIndicator && required) return 'required';
-			if (optionalIndicator && !required) return 'optional';
-			return null;
-		}, [requiredIndicator, optionalIndicator, required]);
+	const Counter = React.useMemo(() => {
+		if (!counter) return null;
+		if (maxLength !== undefined && remaining !== undefined && counterStart !== undefined) {
+			if (remaining <= counterStart) {
+				return (
+					<div className={counterClass}>
+						{ counter({ remaining, max: maxLength }) }
+					</div>
+				);
+			}
+		}
+		return null;
+	}, [counter, counterClass, counterStart, maxLength, remaining]);
 
-		const InputComponent = multiline ? BaseTextArea : BaseInput;
+	const indicator = React.useMemo(() => {
+		if (requiredIndicator && required) return 'required';
+		if (optionalIndicator && !required) return 'optional';
+		return null;
+	}, [requiredIndicator, optionalIndicator, required]);
 
-		return (
-			<div
-				className={classNames(className, { [invalidClass]: !isValid })}
-				id={id.current}
-			>
-				<FieldInfo
-					htmlFor={inputId.current}
-					label={children}
-					indicator={indicator}
-					labelId={labelId.current}
-					labelClass={labelClass}
-					descriptionClass={descriptionClass}
-					descriptionId={descId.current}
-					description={description}
-				/>
-				<div className={groupClass}>
-					{!multiline && createFieldAddons(addonBefore)}
-					<InputComponent
-						ref={ref}
-						value={value}
-						multiline={multiline}
-						autoSize={autoSize}
-						errors={errors}
-						onChange={changeHandler}
-						onDOMChange={onDOMChange}
-						onValidate={validateHandler}
-						id={inputId.current}
-						className={inputClass}
-						aria-describedby={description ? descId.current : undefined}
-						aria-invalid={!isValid}
-						aria-errormessage={!isValid ? errId.current : undefined}
-						// validation props
-						maxLength={maxLength}
-						required={required}
-						type={type}
-						// BaseInput custom validation props
-						validators={validators}
-						validateOnChange={validateOnChange}
-						validateOnDOMChange={validateOnDOMChange}
-						{...inputProps}
-					/>
-					{!multiline && createFieldAddons(addonAfter)}
-				</div>
-				<FieldFeedback
-					className={feedbackClass}
-					errorsId={errId.current}
+	const InputComponent = multiline ? BaseTextArea : BaseInput;
+
+	return (
+		<div
+			className={classNames(className, { [invalidClass]: !isValid })}
+			id={id.current}
+		>
+			<FieldInfo
+				htmlFor={inputId.current}
+				label={children}
+				indicator={indicator}
+				labelId={labelId.current}
+				labelClass={labelClass}
+				descriptionClass={descriptionClass}
+				descriptionId={descId.current}
+				description={description}
+			/>
+			<div className={groupClass}>
+				{ !multiline && createFieldAddons(addonBefore) }
+				<InputComponent
+					ref={ref}
+					value={value}
+					multiline={multiline}
+					autoSize={autoSize}
 					errors={errors}
-					errorsClass={errorsClass}
-				>
-					{feedback}
-					{Counter}
-				</FieldFeedback>
+					onChange={changeHandler}
+					onDOMChange={onDOMChange}
+					onValidate={validateHandler}
+					id={inputId.current}
+					className={inputClass}
+					aria-describedby={(description) ? descId.current : undefined}
+					aria-invalid={!isValid}
+					aria-errormessage={(!isValid) ? errId.current : undefined}
+					// validation props
+					maxLength={maxLength}
+					required={required}
+					type={type}
+					// BaseInput custom validation props
+					validators={validators}
+					validateOnChange={validateOnChange}
+					validateOnDOMChange={validateOnDOMChange}
+					{...inputProps}
+				/>
+				{ !multiline && createFieldAddons(addonAfter) }
 			</div>
-		);
-	},
-);
+			<FieldFeedback
+				className={feedbackClass}
+				errorsId={errId.current}
+				errors={errors}
+				errorsClass={errorsClass}
+			>
+				{ feedback }
+				{ Counter }
+			</FieldFeedback>
+		</div>
+	);
+});
 
 /**
  * An uncontrolled variant of the `TextField` component. The `value` prop doesn't
@@ -225,9 +206,7 @@ export const TextField = React.forwardRef<HTMLInputElement & HTMLTextAreaElement
  * values are submitted with native APIs like
  * [HTMLFormElement.submit()](https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit).
  */
-export const TextFieldUncontrolled = (
-	props: Omit<TextFieldProps, 'value'>,
-): JSX.Element => {
+export const TextFieldUncontrolled = (props: Omit<TextFieldProps, 'value'>): JSX.Element => {
 	const [value, setValue] = React.useState('');
 	return (
 		<TextField
