28630eb05615224d0f2850aaf6444773359128ad
JRebella <jrebella96@gmail.com>
2021-08-09 13:12:14 -0300

    fix: Revert {...attributes} back to summary + temp fix comment

--- packages/react/src/components/BaseSummary/index.tsx
+++ packages/react/src/components/BaseSummary/index.tsx
@@ -16,8 +16,12 @@ export const BaseSummary = React.forwardRef<HTMLElement, BaseSummaryProps>(({
 	children,
 	...attributes
 }: BaseSummaryProps, ref) => (
-	<summary ref={ref}>
-		<div {...attributes}>
+	<summary ref={ref} {...attributes}>
+		{/*
+			This div is a temporary fix for safari 14
+			https://bugs.webkit.org/show_bug.cgi?id=190065 - sumamry can't be flexbox
+		*/}
+		<div style={{ display: 'flex', width: '100%' }}>
 			{ markerPosition === 'left' && marker }
 			{ children }
 			{ markerPosition === 'right' && marker }
