04c8727ca5d42ef23a734d0be11dc9113051b29d
apatil <apatil@wwnorton.com>
2021-08-10 21:33:28 -0400

    refactor(zindex): removed tag component

--- packages/react/src/utilities/index.ts
+++ packages/react/src/utilities/index.ts
@@ -3,7 +3,6 @@ export * from './children';
 export * from './environment';
 export * from './externalClick';
 export * from './forwardedRef';
-export * from './functions';
 export * from './mediaQuery';
 export * from './popper';
 export * from './popperTriggers';
