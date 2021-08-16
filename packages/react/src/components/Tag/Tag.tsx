4f527582d7e3164882c7fdd39132fb003022ff10
Evan Yamanishi <yamanishi1@gmail.com>
2021-08-16 00:20:45 -0400

    chore: export badge and tag types

--- packages/react/src/components/Tag/Tag.tsx
+++ packages/react/src/components/Tag/Tag.tsx
@@ -5,6 +5,8 @@ import { BaseButton } from '../BaseButton';
 import { Icon } from '../Icon';
 import { isLinkElement } from '../../utilities';
 
+export { TagProps } from './types';
+
 export const Tag = React.forwardRef<HTMLElement, TagProps>(({
 	dismissible,
 	baseName = 'nds-tag',
