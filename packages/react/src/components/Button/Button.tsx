d5972504af050db7f895f54ee7d2099e4f8a7854
wguevara-norton <85630200+wguevara-norton@users.noreply.github.com>
2021-10-14 13:28:47 -0500

    visual(button): change the cursor to "not-allowed" when disabled
    * chore(button): disabled cursor when button is disabled or loading, ButtonProps moved to types.ts
    
    * chore(button): disabled cursor when button is disabled, loading prop, mixin cursor-disabled removed
    
    * chore(button): export ButtonProps, ButtonVariant from Button.tsx
    
    * chore(button): pointer-events: none removed when button is disabled

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,61 +1,15 @@
 import React from 'react';
 import classNames from 'classnames';
 import { useForwardedRef } from '../../utilities';
-import { BaseButton, BaseButtonProps } from '../BaseButton';
-import { Icon, IconVariant, SVGIcon } from '../Icon';
+import { BaseButton } from '../BaseButton';
+import { Icon } from '../Icon';
 import { Tooltip } from '../Tooltip';
-import { TooltipCoreProps } from '../Tooltip/types';
 import { LiveRegion, useContentMonitor } from '../LiveRegion';
-import { AllColors } from '../../utilities/color';
 import { BUTTON_NO_NAME } from './errors';
-
-export type ButtonVariant = 'solid' | 'outline' | 'ghost';
-
-export interface ButtonProps extends BaseButtonProps {
-	/**
-	 * Button `children` are required because they are used to provide an accessible
-	 * label for the button. When rendering with `iconOnly`, the children will be
-	 * rendered as an accessible `Tooltip` that labels the button.
-	 */
-	children: BaseButtonProps['children'];
-	/** Button variant conveys the button's level of visual emphasis. */
-	variant?: ButtonVariant;
-	/** An icon to include in the button. */
-	icon?: IconVariant | SVGIcon;
-	/**
-	 * Indicates whether the icon should be to the right of the text. By default,
-	 * the icon is to the left of the text.
-	 */
-	iconRight?: boolean;
-	/**
-	 * Indicates whether the button's contents should only be the icon. When
-	 * `true`, the button's text is still required but will be used as the icon's
-	 * `aria-label`. If no `icon` is specified, this prop has no effect.
-	 */
-	iconOnly?: boolean;
-	/**
-	 * The button's color, restricted to [design system colors](https://wwnorton.github.io/design-system/docs/color),
-	 * excluding `disabled` (prefer the `disabled` prop). Note that an `undefined`
-	 * color will result in the "primary" color being used.
-	 */
-	color?: Exclude<AllColors, 'disabled'>;
-	/** A reference to the inner `<button>` element. */
-	buttonRef?: React.Ref<HTMLButtonElement>;
-	/** The base class name according to BEM conventions. */
-	baseName?: string;
-	/** The className for the Button's icon, if one exists. */
-	iconClass?: string;
-	/** The className for the Button's text, which will be placed in a `<span>` */
-	textClass?: string;
-	/**
-	 * Tooltip props that should be included when the button's children are
-	 * rendered as a tooltip.
-	 */
-	tooltipProps?: Partial<TooltipCoreProps>;
-}
+import { ButtonProps, ButtonVariant } from './types';
 
 /** A button allows a user to perform an action. */
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((
 	{
 		baseName = 'nds-button',
 		variant,
@@ -135,3 +89,9 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((
 		</>
 	);
 });
+
+export {
+	ButtonVariant,
+	ButtonProps,
+	Button,
+};
