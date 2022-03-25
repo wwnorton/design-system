551f80b1245ac72a37b7770a21125bb6a9234096
Evan Yamanishi <yamanishi1@gmail.com>
2022-03-25 13:44:34 -0400

    chore(deps-dev): switch to eslint-config-norton

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,12 @@
 module.exports = {
 	root: true,
 	extends: [
-		'airbnb',
-		'airbnb/hooks',
+		'norton/react',
 	],
 	parserOptions: {
 		project: './tsconfig.json',
 	},
-	settings: {
-		'import/extensions': [
-			'.js',
-			'.jsx',
-			'.mjs',
-			'.ts',
-			'.tsx',
-		],
-		'import/resolver': {
-			node: {
-				extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-			},
-			webpack: {
-				extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-			},
-		},
-		'import/parsers': {
-			'@typescript-eslint/parser': ['.ts', '.tsx'],
-		},
-	},
 	rules: {
-		indent: ['error', 'tab'],
-		'no-tabs': 'off',
-		'lines-between-class-members': [
-			'error',
-			'always',
-			{
-				exceptAfterSingleLine: true,
-			},
-		],
-		'react/function-component-definition': ['error', {
-			namedComponents: 'arrow-function',
-			unnamedComponents: 'function-expression',
-		}],
-		'react/jsx-indent': ['error', 'tab'],
-		'react/jsx-indent-props': ['error', 'tab'],
-		'react/jsx-props-no-spreading': 'off',
-		/**
-		 * Avoid export default to improve TypeScript experience. Applicable in
-		 * JavaScript as well, though less impactful.
-		 * For rationale, see https://basarat.gitbook.io/typescript/main-1/defaultisbad
-		 */
-		'import/prefer-default-export': 'off',
-		'import/no-default-export': 'error',
 		'import/no-extraneous-dependencies': [
 			'error',
 			{
@@ -62,45 +18,28 @@ module.exports = {
 				],
 			},
 		],
-		'import/extensions': [
-			'error',
-			'ignorePackages',
-			{
-				js: 'never',
-				mjs: 'never',
-				jsx: 'never',
-				ts: 'never',
-				tsx: 'never',
-			},
-		],
+		'react/function-component-definition': ['error', {
+			namedComponents: 'arrow-function',
+			unnamedComponents: 'function-expression',
+		}],
+		// Most of our components extend an HTML element's interface, and we use
+		// props spreading to allow access to that interface. This should only be
+		// done for low-level component libraries.
+		'react/jsx-props-no-spreading': 'off',
 	},
 	overrides: [
 		// TypeScript
 		{
 			files: ['*.ts', '*.tsx'],
 			extends: [
-				'plugin:import/typescript',
-				'plugin:@typescript-eslint/recommended',
 				// 'plugin:@typescript-eslint/recommended-requiring-type-checking',
 			],
 			rules: {
-				indent: 'off',
-				'no-tabs': 'off',
-				'no-use-before-define': 'off',
-				'@typescript-eslint/indent': ['error', 'tab'],
-				'@typescript-eslint/no-empty-interface': 'off',
-				'@typescript-eslint/no-use-before-define': ['error'],
-				'react/jsx-filename-extension': [
-					'error',
-					{
-						extensions: ['.jsx', '.tsx'],
-					},
-				],
+				// Disable prop-types since ESLint has trouble with TypeScript-defined props
+				// and TypeScript provides better props typing anyway
 				'react/prop-types': 'off',
-				'react/static-property-placement': [
-					'error',
-					'static public field',
-				],
+				'react/require-default-props': 'off',
+				'react/default-props-match-prop-types': 'off',
 			},
 		},
 		// Storybook
