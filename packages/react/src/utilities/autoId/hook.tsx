f6969b4d84323b3c646c761c5f344afbbdac101e
Wilmer Guevara - Norton <wilmerguevara-norton@Wilmers-MacBook-Pro.local>
2022-05-27 09:26:08 -0500

    chore(tabs): add useId hook utility

---
+++ packages/react/src/utilities/autoId/hook.tsx
@@ -0,0 +1,46 @@
+// This implementation is heavily inspired by @accessible/use-id implementation
+import React, { useState } from 'react';
+import { useLayoutEffect } from '../isomorphicLayoutEffect';
+
+export interface BaseIdGeneratorProps {
+	/**
+	 * Allows you to provide your own id as a fallback
+	 */
+	providedId?: number | string | undefined | null;
+}
+
+let ID = 0;
+// eslint-disable-next-line no-return-assign
+const genId = (): number => ID += 1;
+let serverHandoffComplete = false;
+
+/**
+ * useId
+ *
+ * Autogenerate IDs to facilitate WAI-ARIA and server rendering.
+ *
+ * Note: The returned ID will initially be `null` and will update after a
+ * component mounts. Users may need to supply their own ID if they need
+ * consistent values for SSR.
+ *
+ */
+export function useId(providedId?: BaseIdGeneratorProps) {
+	const [id, setId] = useState(
+		providedId ?? (serverHandoffComplete ? genId() : null),
+	);
+
+	useLayoutEffect(() => {
+		if (id === null) {
+			if (typeof (React as any).useId === 'function') {
+				setId((React as any).useId());
+			} else {
+				setId(genId());
+			}
+		}
+
+		serverHandoffComplete = true;
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return providedId ?? id ?? undefined;
+}
