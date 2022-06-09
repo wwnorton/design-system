6c9cf25b576d80e129be0fab2f9d5a568bc2f3ed
Evan Yamanishi <yamanishi1@gmail.com>
2022-06-09 11:03:28 -0400

    chore: rename hook directory for consistency

--- packages/react/src/utilities/index.ts
+++ packages/react/src/utilities/index.ts
@@ -1,11 +1,11 @@
 // Public utilities
-export * from './autoId';
 export * from './children';
 export * from './environment';
 export * from './externalClick';
 export * from './forwardedRef';
-export * from './link';
+export * from './id';
 export * from './isomorphicLayoutEffect';
+export * from './link';
 export * from './mediaQuery';
 export * from './popper';
 export * from './popperTriggers';
