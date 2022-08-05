1877d403a3948456b7f1c283b373899269c22ac0
Evan Yamanishi <yamanishi1@gmail.com>
2022-08-05 12:10:36 -0400

    docs: swizzle our own admonition to use the NDS Callout
    https://docusaurus.io/docs/markdown-features/admonitions#customizing-admonitions

---
+++ website/src/theme/Admonition/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+	Callout, CalloutError, CalloutSuccess, CalloutWarning,
+} from '@wwnds/react';
+
+interface AdmonitionProps {
+	type: string;
+	children?: React.ReactNode;
+	title?: string;
+}
+
+const Admonition = ({ children, type, title }: AdmonitionProps) => {
+	const CalloutComponent = React.useMemo(() => {
+		switch (type) {
+			case 'tip': return CalloutSuccess;
+			case 'caution': return CalloutWarning;
+			case 'danger': return CalloutError;
+			default: return Callout;
+		}
+	}, [type]);
+
+	return (
+		<CalloutComponent title={title} border="left">
+			{ children }
+		</CalloutComponent>
+	);
+};
+
+export default Admonition;
