203223fc12684c4582be4c46ad7bddb08ec44324
Evan Yamanishi <yamanishi1@gmail.com>
2022-08-30 18:39:32 -0400

    refactor(button): clarify error and update link

--- packages/react/src/components/Button/errors.ts
+++ packages/react/src/components/Button/errors.ts
@@ -1,3 +1,3 @@
 export const BUTTON_NO_NAME = 'Button components must always have a valid accessible name.'
-	+ ' This means that your button is missing `children` or an ARIA name.'
-	+ ' See https://w3c.github.io/aria-practices/#names_and_descriptions.';
+	+ ' This can be provided via children or an ARIA name.'
+	+ ' See https://www.w3.org/WAI/ARIA/apg/practices/names-and-descriptions/ for more details.';
