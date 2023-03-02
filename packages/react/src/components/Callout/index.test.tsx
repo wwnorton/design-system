dcc734532560bbb63b21243b420ffe77e22aceeb
Evan Yamanishi <yamanishi1@gmail.com>
2023-03-02 17:45:18 -0500

    test(callout): add tests for accessible naming conditions

---
+++ packages/react/src/components/Callout/index.test.tsx
@@ -0,0 +1,28 @@
+import test from 'ava';
+import React from 'react';
+import { cleanup, render, screen } from '@testing-library/react';
+import { Callout } from '.';
+
+test.afterEach(cleanup);
+
+// The default <aside> is an ARIA landmark and should be labelled by the title
+test('a Callout uses its title for its accessible name', async (t) => {
+	const name = 'Foo';
+	render(<Callout title={name} />);
+
+	t.truthy(screen.queryByRole('complementary', { name }));
+});
+
+// <div> callouts are _not_ ARIA landmarks, and should therefore not have an accessible name
+test('a div Callout does not use its title as an accessible name', async (t) => {
+	const name = 'Foo';
+	render(<Callout title={name} tag="div" />);
+
+	t.falsy(screen.queryByRole('complementary', { name }));
+});
+
+test('a Callout without a title is not rendered as a landmark', async (t) => {
+	render(<Callout />);
+
+	t.falsy(screen.queryByRole('complementary'));
+});
