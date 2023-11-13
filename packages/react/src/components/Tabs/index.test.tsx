ed4933c2eed48f0ca962ad33e10effe085ad35df
Carlos Frias <cfrias@wwnorton.com>
2023-11-13 16:55:24 -0300

    chore: include tests for Tabs keyboard nav

--- packages/react/src/components/Tabs/index.test.tsx
+++ packages/react/src/components/Tabs/index.test.tsx
@@ -1,8 +1,9 @@
 import test from 'ava';
 import React from 'react';
 import {
-	cleanup, render, fireEvent, screen,
+	cleanup, render, fireEvent, screen, waitFor,
 } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import {
 	Tabs, Tab, TabList, TabPanels, TabPanel,
 } from '.';
@@ -97,3 +98,96 @@ test('Controlled: clicking on external controls, switches panels', (t) => {
 		areTabAndPanelSelected('Cats'),
 	);
 });
+
+test('Tabs Keyboard Navigation', async (t) => {
+	render(
+		<Tabs>
+			<TabList>
+				<Tab>Tab 1</Tab>
+				<Tab>Tab 2</Tab>
+				<Tab>Tab 3</Tab>
+			</TabList>
+			<TabPanels>
+				<TabPanel>Content 1</TabPanel>
+				<TabPanel>Content 2</TabPanel>
+				<TabPanel>Content 3</TabPanel>
+			</TabPanels>
+		</Tabs>,
+	);
+
+	const tab1 = screen.getByRole('tab', { name: 'Tab 1' });
+	const tab2 = screen.getByRole('tab', { name: 'Tab 2' });
+	const tab3 = screen.getByRole('tab', { name: 'Tab 3' });
+
+	tab1.focus();
+
+	const tabList = screen.getByRole('tablist');
+
+	// Right Arrow moves to next
+	fireEvent.keyDown(tabList, { key: 'ArrowRight' });
+	await waitFor(() => {
+		t.true(document.activeElement === tab2);
+	});
+
+	// Left Arrow moves to previous
+	fireEvent.keyDown(tabList, { key: 'ArrowLeft' });
+	await waitFor(() => {
+		t.true(document.activeElement === tab1);
+	});
+
+	// End moves to last
+	fireEvent.keyDown(tabList, { key: 'End' });
+	await waitFor(() => {
+		t.true(document.activeElement === tab3);
+	});
+
+	// Home moves to first
+	fireEvent.keyDown(tabList, { key: 'Home' });
+	await waitFor(() => {
+		t.true(document.activeElement === tab1);
+	});
+
+	// Left on first wraps to last
+	fireEvent.keyDown(tabList, { key: 'ArrowLeft' });
+	await waitFor(() => {
+		t.true(document.activeElement === tab3);
+	});
+
+	// Right on last wraps to first
+	fireEvent.keyDown(tabList, { key: 'ArrowRight' });
+	await waitFor(() => {
+		t.true(document.activeElement === tab1);
+	});
+});
+
+// test('When panel does not contain focus-able elements, focus goes to container', async (t) => {
+// 	render(
+// 		<Tabs>
+// 			<TabList>
+// 				<Tab>Tab 1</Tab>
+// 				<Tab>Tab 2</Tab>
+// 			</TabList>
+// 			<TabPanels>
+// 				<TabPanel>Content 1</TabPanel>
+// 				<TabPanel>Content 2</TabPanel>
+// 			</TabPanels>
+// 		</Tabs>,
+// 	);
+
+// 	const tab1 = screen.getByRole('tab', { name: 'Tab 1' });
+// 	const tabPanel1 = screen.getByRole('tabpanel', { name: 'Tab 1' });
+
+// 	// First tab event moves focus to First Tab
+// 	userEvent.tab();
+// 	await waitFor(() => {
+// 		console.log(document.activeElement);
+// 		t.true(document.activeElement === tab1);
+// 	});
+
+// 	// Second tab event should move the focus to the panel
+// 	userEvent.tab();
+// 	await waitFor(() => {
+// 		console.log(document.activeElement);
+// 		t.true(document.activeElement === tabPanel1);
+// 	});
+// });
