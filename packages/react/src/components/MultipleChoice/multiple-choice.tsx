99f6ed56ed69f4b5bf4e5b5bec20c43f164f2d4a
Carlos Frias <cfrias@wwnorton.com>
2024-03-20 15:07:18 -0300

    chore: wip

--- packages/react/src/components/MultipleChoice/multiple-choice.tsx
+++ packages/react/src/components/MultipleChoice/multiple-choice.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { RadioGroup } from '../Radio';
-import { ResponseIndicator } from '../ResponseIndicator';
-import { AnswerChoice } from './answer-choice';
-import { Instructions } from './instructions';
-import { Intro } from './intro';
-import { Stem } from './stem';
-import { styles } from './styles';
-import { LabelType, OnSelectInput } from './types';
-import { resolveLabelType } from './utils';
+import React from "react";
+import { RadioGroup } from "../Radio";
+import { ResponseIndicator } from "../ResponseIndicator";
+import { AnswerChoice } from "./answer-choice";
+import { Instructions } from "./instructions";
+import { Intro } from "./intro";
+import { Stem } from "./stem";
+import { styles } from "./styles";
+import { LabelType, OnSelectInput } from "./types";
+import { resolveLabelType } from "./utils";
 
 interface MultipleChoiceProps {
 	stem: string | React.ReactElement<void, typeof Stem>;
@@ -18,24 +18,31 @@ interface MultipleChoiceProps {
 	 * @default 'lower-alpha'
 	 */
 	labelType?: LabelType;
-	status: 'correct' | 'incorrect' | 'unanswered';
+	status: "correct" | "incorrect" | "unanswered";
 	onSelect?: (input: OnSelectInput) => void;
 	selected?: number;
+	// TODO: support styling pieces
 }
 
 export const MultipleChoice = ({
 	stem,
 	intro,
 	instructions,
-	labelType = 'lower-alpha',
+	labelType = "lower-alpha",
 	choices,
 	status,
 	onSelect,
 	selected,
 }: MultipleChoiceProps) => {
-	const introElement = typeof intro === 'string' ? <Intro>{intro}</Intro> : intro;
-	const stemElement = typeof stem === 'string' ? <Stem>{stem}</Stem> : stem;
-	const instructionsElement = typeof instructions === 'string' ? <Instructions>{instructions}</Instructions> : instructions;
+	const introElement =
+		typeof intro === "string" ? <Intro>{intro}</Intro> : intro;
+	const stemElement = typeof stem === "string" ? <Stem>{stem}</Stem> : stem;
+	const instructionsElement =
+		typeof instructions === "string" ? (
+			<Instructions>{instructions}</Instructions>
+		) : (
+			instructions
+		);
 
 	return (
 		<div>
@@ -44,57 +51,54 @@ export const MultipleChoice = ({
 			{instructionsElement}
 			<div>
 				<RadioGroup label={undefined}>
-					{
-						choices.map(
-							(choice, index) => {
-								const isCorrect = status === 'correct' && index === selected;
-								const isIncorrect = status === 'incorrect' && index === selected;
-								const label = resolveLabelType(labelType, index);
+					{choices.map((choice, index) => {
+						const isCorrect = status === "correct" && index === selected;
+						const isIncorrect = status === "incorrect" && index === selected;
+						const label = resolveLabelType(labelType, index);
 
-								let feedback: React.ReactNode = null;
-								if (isCorrect) {
-									feedback = (
-										<ResponseIndicator
-											className={styles.feedback}
-											withIcon={false}
-											variant="correct"
-										/>
-									);
-								} else if (isIncorrect) {
-									feedback = (
-										<ResponseIndicator
-											className={styles.feedback}
-											withIcon={false}
-											variant="incorrect"
-										/>
-									);
-								} else {
-									feedback = <div className={styles.feedback} />;
-								}
+						let feedback: React.ReactNode = null;
+						if (isCorrect) {
+							feedback = (
+								<ResponseIndicator
+									className={styles.feedback}
+									withIcon={false}
+									variant="correct"
+								/>
+							);
+						} else if (isIncorrect) {
+							feedback = (
+								<ResponseIndicator
+									className={styles.feedback}
+									withIcon={false}
+									variant="incorrect"
+								/>
+							);
+						} else {
+							feedback = <div className={styles.feedback} />;
+						}
 
-								const checked = index === selected;
+						const checked = index === selected;
 
-								// TODO: use grid to solve issues with incorrect response
-								// indicator
-								return (
-									<div key={label} className={styles.choice}>
-										{feedback}
-										<div>
-
-											<AnswerChoice
-												label={label}
-												checked={checked}
-												value={index}
-												onSelect={onSelect}
-											>
-												{choice}
-											</AnswerChoice>
-										</div>
-									</div>
-								);
-							},
-						)
-					}
+						// TODO: think a way to allow control of response indicator layout
+						//
+						// TODO: use grid to solve issues with incorrect response
+						// indicator
+						return (
+							<div key={label} className={styles.choice}>
+								{feedback}
+								<div>
+									<AnswerChoice
+										label={label}
+										checked={checked}
+										value={index}
+										onSelect={onSelect}
+									>
+										{choice}
+									</AnswerChoice>
+								</div>
+							</div>
+						);
+					})}
 				</RadioGroup>
 			</div>
 		</div>
