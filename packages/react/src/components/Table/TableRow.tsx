274dab78837f6be3674cd95d9f4b5fa0302ed9cc
Ariel Ferro <aferro@wwnorton.com>
2024-03-20 14:52:43 -0300

    chore: implemented table row selection

--- packages/react/src/components/Table/TableRow.tsx
+++ packages/react/src/components/Table/TableRow.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import classNames from 'classnames';
-import { TableRowProps } from './types';
+import React from "react";
+import classNames from "classnames";
+import { TableRowProps } from "./types";
+import { useTableState } from './context';
+import { Checkbox } from '../Checkbox';
+import { useId } from '../../utilities';
 
 export const TableRow: React.FC<TableRowProps> = ({
-	baseName = 'nds-table-row',
+	baseName = "nds-table-row",
 	headerClass = `${baseName}__header`,
 	sectionHeaderClass = `${baseName}__header--section`,
 	isHeader,
 	isSectionHeader,
 	children,
 }) => {
+
+	const { selectable, onSelect, selected, onSelected, isSelectedAll } = useTableState();
+	const uniqueId = useId();
+
 	const trClassName = classNames(baseName, {
 		[`${headerClass}`]: isHeader,
 		[`${sectionHeaderClass}`]: isSectionHeader,
 	});
+const isSelected =isSelectedAll  || selected?.includes(`${uniqueId}`);
+const showCheckbox = selectable && onSelect && !isHeader && !isSectionHeader
 
 	return (
 		<tr className={trClassName}>
+			{ showCheckbox ? (
+			<td>
+<Checkbox id={uniqueId} checked={isSelected} disabled={isSelectedAll} onChange={onSelected} />
+			</td>) : null }
 			{children}
 		</tr>
 	);
