dd9dbcb375bae4c7e48cc363e1ac0fbf5c4203b3
Evan Yamanishi <eyamanishi@wwnorton.com>
2024-03-22 13:23:42 -0400

    chore(docs): ignore import errors for swizzled components

--- website/src/theme/.eslintrc.js
+++ website/src/theme/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
 	rules: {
 		"import/prefer-default-export": "error",
 		"import/no-default-export": "off",
+		"import/no-extraneous-dependencies": "off",
 		"react/no-array-index-key": "off",
 		"@typescript-eslint/no-explicit-any": "off",
 	},
