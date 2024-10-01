655c3ccd2b9ac16e16bf22486cdd363f145da92b
Carlos Frias <cfrias@wwnorton.com>
2024-10-01 16:08:52 -0300

    chore: add faker

--- packages/react/src/components/Table/Table.stories.tsx
+++ packages/react/src/components/Table/Table.stories.tsx
@@ -1,5 +1,9 @@
+import { faker } from '@faker-js/faker';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Table } from './Table';
+import { TableDataCell } from './types';
+
+faker.seed(123);
 
 const meta = {
 	title: 'Table',
@@ -10,15 +14,29 @@ export default meta;
 
 type Story = StoryObj<typeof Table>;
 
+const ROWS = 10;
+
 export const Default: Story = {
 	args: {
 		isSortable: true,
 		data: {
-			headers: [{ children: 'Name' }, { children: 'Age' }, { children: 'Country' }],
-			rows: [
-				[{ value: 'John Doe' }, { value: 32 }, { value: 'Canada' }],
-				[{ value: 'Jane Doe' }, { value: 31 }, { value: 'USA' }],
+			headers: [
+				{ children: 'Name' },
+				{ children: 'Age' },
+				{
+					children: 'Country',
+					sorter: (a, b) => {
+						return (b as string).length - (a as string).length;
+					},
+				},
 			],
+			rows: Array(ROWS)
+				.fill(null)
+				.map<TableDataCell[]>(() => [
+					{ value: faker.person.fullName() },
+					{ value: faker.number.int({ min: 20, max: 60 }) },
+					{ value: faker.location.country() },
+				]),
 		},
 	},
 };
