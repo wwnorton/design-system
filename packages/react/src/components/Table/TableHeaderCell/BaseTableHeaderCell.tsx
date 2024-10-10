97a1ea462753ec832d31ec1c50e83f4753948592
Carlos Frias <cfrias@wwnorton.com>
2024-10-10 17:12:00 -0300

    chore(table): add sort button styles

--- packages/react/src/components/Table/TableHeaderCell/BaseTableHeaderCell.tsx
+++ packages/react/src/components/Table/TableHeaderCell/BaseTableHeaderCell.tsx
@@ -1,5 +1,66 @@
-import React, { useMemo } from 'react';
+import React from 'react';
+import classNames from 'classnames';
 import { BaseTableHeaderCellProps } from './types';
+import { Icon } from '../../Icon';
+
+const css = {
+	sortButton: {
+		base: 'nds-table-sort-button',
+		asc: 'nds-table-sort-button__icon--asc',
+		desc: 'nds-table-sort-button__icon--desc',
+		icon: 'nds-table-sort-button__icon',
+	},
+};
+
+interface SortButtonProps {
+	order: number | undefined;
+	onClick: () => void;
+	children?: React.ReactNode;
+}
+
+export const SortButton = ({ order, onClick, children }: SortButtonProps) => {
+	const iconClassName = classNames(css.sortButton.icon, {
+		[css.sortButton.asc]: order === 2,
+		[css.sortButton.desc]: order === 0,
+	});
+
+	let icon: React.ReactNode;
+	switch (order) {
+		case 0:
+			icon = <Icon size="1rem" variant="caret-down" />;
+			break;
+		case 2:
+			icon = <Icon size="1rem" variant="caret-up" />;
+			break;
+		default:
+			icon = (
+				<>
+					<Icon
+						viewBox="6 6 12 12"
+						width="1rem"
+						height="0.5rem"
+						style={{ width: '1rem' }}
+						variant="caret-up"
+					/>
+					<Icon
+						viewBox="6 6 12 12"
+						width="1rem"
+						height="0.5rem"
+						style={{ width: '1rem' }}
+						variant="caret-down"
+					/>
+				</>
+			);
+			break;
+	}
+
+	return (
+		<button className={css.sortButton.base} type="button" onClick={onClick}>
+			{children}
+			<div className={iconClassName}>{icon}</div>
+		</button>
+	);
+};
 
 export const BaseTableHeaderCell = ({
 	order,
@@ -7,35 +68,14 @@ export const BaseTableHeaderCell = ({
 	children,
 	...other
 }: BaseTableHeaderCellProps) => {
-	const sortButton = useMemo(() => {
-		if (order === undefined || !onSort) {
-			return null;
-		}
-
-		let orderText: string;
-		switch (order) {
-			case 0:
-				orderText = 'desc';
-				break;
-			case 2:
-				orderText = 'asc';
-				break;
-			default:
-				orderText = 'default';
-				break;
-		}
-
-		return (
-			<button type="button" onClick={onSort}>
-				sort {orderText}
-			</button>
+	let content = children;
+	if (onSort) {
+		content = (
+			<SortButton order={order} onClick={onSort}>
+				{children}
+			</SortButton>
 		);
-	}, [order, onSort]);
+	}
 
-	return (
-		<th {...other}>
-			{children}
-			{sortButton}
-		</th>
-	);
+	return <th {...other}>{content}</th>;
 };
